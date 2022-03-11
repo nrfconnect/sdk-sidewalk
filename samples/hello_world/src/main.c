f7e6d3a636d8697e44bd9c0b5d31de69f3e65899
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-03-11 15:12:20 +0100

    Implementation of the sid pal storage kv interface

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -1,13 +1,18 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2022 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <sid_pal_storage_kv_ifc.h>
 
 void main(void)
 {
 	printk("Hello Sidewalk World! %s\n", CONFIG_BOARD);
+	if(SID_ERROR_NONE == sid_pal_storage_kv_init())
+	{
+		printk("NVM memory is ready!\n");
+	}
 }
