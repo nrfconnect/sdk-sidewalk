197ca6d51cb4191a0a309c7001a112ec49c91e23
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-03-11 13:33:18 +0100

    Use nrf log system in main
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/hello_sidewalk/src/main.c
+++ samples/hello_sidewalk/src/main.c
@@ -4,22 +4,20 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-#include <zephyr.h>
-#include <sys/printk.h>
 #include <sid_pal_storage_kv_ifc.h>
-
-#include "sid_pal_log_ifc.h"
+#include <sid_pal_log_ifc.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(app, LOG_LEVEL_DBG);
 
 void main(void)
 {
-	printk("Hello Sidewalk World! %s\n", CONFIG_BOARD);
+	LOG_INF("Hello Sidewalk World! %s\n", CONFIG_BOARD);
+
 	if (SID_ERROR_NONE == sid_pal_storage_kv_init()) {
-		printk("NVM memory is ready!\n");
+		LOG_INF("NVM memory is ready!\n");
 	}
 
-	LOG_INF("Hello Sidewalk World! %s\n", CONFIG_BOARD);
+	
 	SID_PAL_LOG_INFO("Hello Sidewalk Log! %d", 2022);
 }
