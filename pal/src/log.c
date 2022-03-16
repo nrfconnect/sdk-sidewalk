fda78c7924b49dd663d779d89dffaf0f1b24e502
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-03-16 10:35:23 +0100

    Use log deferred mode
    Enables log_buffered_cnt in flush.
    Implement log_strdup as log_push_string.
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- pal/src/log.c
+++ pal/src/log.c
@@ -12,8 +12,9 @@
 #include <logging/log_ctrl.h>
 LOG_MODULE_REGISTER(sid_log, LOG_LEVEL_DBG);
 
-#define MSG_LENGTH_MAX (CONFIG_SIDEWALK_LOG_MSG_LENGTH_MAX)
 #define LOG_FLUSH_SLEEP_PERIOD K_MSEC(5)
+
+#define MSG_LENGTH_MAX (CONFIG_SIDEWALK_LOG_MSG_LENGTH_MAX)
 
 void sid_pal_log(sid_pal_log_severity_t severity, uint32_t num_args, const char *fmt, ...)
 {
@@ -21,11 +22,13 @@ void sid_pal_log(sid_pal_log_severity_t severity, uint32_t num_args, const char 
 
 	va_list args;
 	int print_len = 0;
-	char msg[MSG_LENGTH_MAX] = "";
+	char msg_buff[MSG_LENGTH_MAX] = "";
 
 	va_start(args, fmt);
-	print_len = vsnprintk(msg, sizeof(msg), fmt, args);
+	print_len = vsnprintk(msg_buff, sizeof(msg_buff), fmt, args);
 	va_end(args);
+
+	const char *msg = log_strdup(msg_buff);
 
 	switch (severity) {
 	case SID_PAL_LOG_SEVERITY_ERROR:
@@ -53,16 +56,17 @@ void sid_pal_log(sid_pal_log_severity_t severity, uint32_t num_args, const char 
 
 void sid_pal_log_flush(void)
 {
+#ifndef CONFIG_LOG_MODE_MINIMAL
+	/* Note: log_buffered_cnt is not supported in minimal log mode. */
 	while (log_buffered_cnt()) {
 		k_sleep(LOG_FLUSH_SLEEP_PERIOD);
 	}
+#endif
 }
 
 char const *sid_pal_log_push_str(char *string)
 {
-	LOG_WRN("%s - not implemented (optional).", __func__);
-
-	return string;
+	return log_strdup(string);
 }
 
 bool sid_pal_log_get_log_buffer(struct sid_pal_log_buffer *const log_buffer)
