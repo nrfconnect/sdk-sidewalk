202f87c549965134a0512bacd8122bb3eca08599
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-03-23 11:19:47 +0100

    Timer interface implementation

--- samples/hello_sidewalk/src/main.c
+++ samples/hello_sidewalk/src/main.c
@@ -9,6 +9,8 @@
 #include <sid_pal_mfg_store_ifc.h>
 #include <storage/flash_map.h>
 #include <sid_pal_assert_ifc.h>
+#include <sid_pal_timer_ifc.h>
+
 #include <logging/log.h>
 #include <zephyr.h>
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
@@ -24,9 +26,21 @@ static const sid_pal_mfg_store_region_t mfg_store_region = {
 	.addr_end = (uintptr_t)(FLASH_AREA_OFFSET(mfg_storage) + FLASH_AREA_SIZE(mfg_storage)),
 };
 
+sid_pal_timer_t sidewalk_timer;
+char *sidewalk_timer_str = "Hello Sidewalk timer!";
+
+struct sid_timespec sidewalk_timer_duration = { .tv_sec = 5 };
+struct sid_timespec sidewalk_timer_period = { .tv_sec = 10 };
+
+void sidewalk_timer_cb(void *arg, sid_pal_timer_t *originator)
+{
+	LOG_INF("sidewalk_timer_cb says \"%s\"\n", (char *)arg);
+}
+
 void main(void)
 {
 	uint32_t mfg_ver;
+	sid_error_t erc;
 
 	LOG_INF("Hello Sidewalk World! %s\n", CONFIG_BOARD);
 
@@ -46,6 +60,19 @@ void main(void)
 		sid_pal_mfg_store_dev_id_get(dev_id);
 		SID_PAL_LOG_INFO("Mfg dev ID:");
 		SID_PAL_HEXDUMP(SID_PAL_LOG_SEVERITY_INFO, dev_id, sizeof(dev_id));
+	}
+
+	erc = sid_pal_timer_init(&sidewalk_timer, sidewalk_timer_cb, sidewalk_timer_str);
+	if (SID_ERROR_NONE == erc) {
+		erc = sid_pal_timer_arm(&sidewalk_timer, SID_PAL_TIMER_PRIO_CLASS_LOWPOWER,
+					&sidewalk_timer_duration, &sidewalk_timer_period);
+		if (SID_ERROR_NONE == erc) {
+			SID_PAL_LOG_INFO("Timer is started and %sarmed!", sid_pal_timer_is_armed(&sidewalk_timer) ? "" : "dis");
+		} else {
+			SID_PAL_LOG_WARNING("Timer cannot be armed [erc=%d].", erc);
+		}
+	}else{
+		SID_PAL_LOG_WARNING("Timer initialization error=%d.", erc);
 	}
 
 	SID_PAL_ASSERT(true);
--- tests/sidewalk_tests/src/sidewalk_ifc_test.c
+++ tests/sidewalk_tests/src/sidewalk_ifc_test.c
@@ -6,6 +6,7 @@
 #include <unity.h>
 #include <sid_pal_storage_kv_ifc.h>
 #include <sid_pal_mfg_store_ifc.h>
+#include <sid_pal_timer_ifc.h>
 #include <sid_pal_assert_ifc.h>
 #include <mock_nvs.h>
 #include <mock_flash.h>
@@ -14,7 +15,14 @@
 #define GROUP_ID_TEST_OK        0
 #define GROUP_ID_TEST_NOK       9
 
+#define SID_PAL_TIMER_PRIO_CLASS_TOO_BIG        (9)
+#define SID_PAL_TIMER_PRIO_CLASS_TOO_SMALL      (-9)
+
 static uint8_t test_data_buffer[512];
+
+static sid_pal_timer_t *p_null_timer = NULL;
+static sid_pal_timer_t test_timer;
+static int test_timer_arg;
 
 void setUp(void)
 {
@@ -25,9 +33,9 @@ void setUp(void)
 * ****************************************************************/
 void test_sid_pal_storage_kv_init(void)
 {
-	__wrap_nvs_init_ExpectAnyArgsAndReturn(-EINVAL);
+	__wrap_nvs_mount_ExpectAnyArgsAndReturn(-EINVAL);
 	TEST_ASSERT_EQUAL(SID_ERROR_GENERIC, sid_pal_storage_kv_init());
-	__wrap_nvs_init_ExpectAnyArgsAndReturn(0);
+	__wrap_nvs_mount_ExpectAnyArgsAndReturn(0);
 	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_init());
 }
 
@@ -111,7 +119,7 @@ void test_sid_pal_storage_kv_group_delete(void)
 	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_ERASE_FAIL, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
 
 	__wrap_nvs_clear_ExpectAnyArgsAndReturn(0);
-	__wrap_nvs_init_ExpectAnyArgsAndReturn(0);
+	__wrap_nvs_mount_ExpectAnyArgsAndReturn(0);
 	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
 }
 
@@ -203,6 +211,144 @@ void test_sid_pal_mfg_storage_sn_get(void)
 
 	// No serial number.
 	TEST_ASSERT_FALSE(sid_pal_mfg_store_serial_num_get(serial_num));
+}
+
+/******************************************************************
+* sid_pal_timer_ifc
+* ****************************************************************/
+void timer_cb(void *arg, sid_pal_timer_t *originator)
+{
+
+}
+
+static void timer_init(void)
+{
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_init(&test_timer, timer_cb, &test_timer_arg));
+	TEST_ASSERT_TRUE(test_timer.is_initialized);
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_EQUAL(&test_timer_arg, test_timer.callback_arg);
+	TEST_ASSERT_EQUAL(timer_cb, test_timer.callback);
+}
+
+static void timer_deinit(void)
+{
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_deinit(&test_timer));
+	TEST_ASSERT_FALSE(test_timer.is_initialized);
+	TEST_ASSERT_NULL(test_timer.callback_arg);
+	TEST_ASSERT_NULL(test_timer.callback);
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_FALSE(sid_pal_timer_is_armed(&test_timer));
+}
+
+void test_sid_pal_timer_init_deinit(void)
+{
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_init(NULL, NULL, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_init(&test_timer, NULL, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_init(p_null_timer, timer_cb, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_deinit(p_null_timer));
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_init(&test_timer, timer_cb, NULL));
+	TEST_ASSERT_TRUE(test_timer.is_initialized);
+	TEST_ASSERT_NULL(test_timer.callback_arg);
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_EQUAL(timer_cb, test_timer.callback);
+	TEST_ASSERT_EQUAL(SID_ERROR_ALREADY_INITIALIZED, sid_pal_timer_init(&test_timer, timer_cb, NULL));
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_deinit(&test_timer));
+	TEST_ASSERT_FALSE(test_timer.is_initialized);
+	TEST_ASSERT_NULL(test_timer.callback_arg);
+	TEST_ASSERT_NULL(test_timer.callback);
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_EQUAL(0, test_timer.is_armed);
+
+	timer_init();
+	timer_deinit();
+}
+
+void test_sid_pal_timer_arm(void)
+{
+	struct sid_timespec when = { .tv_sec = 5 };
+	struct sid_timespec period = { .tv_sec = 5 };
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_arm(p_null_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, NULL, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_arm(p_null_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, NULL, NULL));
+
+	TEST_ASSERT_EQUAL(SID_ERROR_UNINITIALIZED, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+
+	timer_init();
+
+	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_TOO_BIG, &when, NULL));
+	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_TOO_SMALL, &when, NULL));
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_NOT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_cancel(&test_timer));
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, &period));
+	TEST_ASSERT_TRUE(test_timer.is_periodic);
+	TEST_ASSERT_NOT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+
+	timer_deinit();
+}
+
+void test_sid_pal_timer_is_armed(void)
+{
+	struct sid_timespec when = { .tv_sec = 5 };
+
+	TEST_ASSERT_FALSE(sid_pal_timer_is_armed(p_null_timer));
+	TEST_ASSERT_FALSE(sid_pal_timer_is_armed(&test_timer));
+
+	timer_init();
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+	TEST_ASSERT_TRUE(sid_pal_timer_is_armed(&test_timer));
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_cancel(&test_timer));
+	TEST_ASSERT_FALSE(sid_pal_timer_is_armed(&test_timer));
+
+	timer_deinit();
+}
+
+void test_sid_pal_timer_cancel(void)
+{
+	struct sid_timespec when = { .tv_sec = 5 };
+	struct sid_timespec period = { .tv_sec = 5 };
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_timer_cancel(p_null_timer));
+	TEST_ASSERT_EQUAL(SID_ERROR_UNINITIALIZED, sid_pal_timer_cancel(&test_timer));
+
+	timer_init();
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, &period));
+	TEST_ASSERT_TRUE(test_timer.is_initialized);
+	TEST_ASSERT_TRUE(test_timer.is_periodic);
+	TEST_ASSERT_NOT_EQUAL(0, test_timer.is_armed);
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_cancel(&test_timer));
+	TEST_ASSERT_TRUE(test_timer.is_initialized);
+	TEST_ASSERT_FALSE(test_timer.is_periodic);
+	TEST_ASSERT_EQUAL(0, test_timer.is_armed);
+
+	timer_deinit();
+}
+
+void test_sid_pal_timer_is_armed_deinit(void)
+{
+	struct sid_timespec when = { .tv_sec = 5 };
+
+	timer_init();
+
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_timer_arm(&test_timer, SID_PAL_TIMER_PRIO_CLASS_PRECISE, &when, NULL));
+	TEST_ASSERT_TRUE(sid_pal_timer_is_armed(&test_timer));
+
+	timer_deinit();
+
+	TEST_ASSERT_FALSE(sid_pal_timer_is_armed(&test_timer));
 }
 
 /* It is required to be added to each test. That is because unity is using
