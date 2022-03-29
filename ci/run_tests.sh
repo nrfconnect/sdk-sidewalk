f5f4452a41ae7bfe40d5a621130f9ebd08787c34
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-03-29 16:10:12 +0200

    Add Twister test runer configuration
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

---
+++ ci/run_tests.sh
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+#
+
+$ZEPHYR_BASE/scripts/twister -v -p native_posix -p nrf52840dk_nrf52840 -C -T $ZEPHYR_BASE/../sidewalk
