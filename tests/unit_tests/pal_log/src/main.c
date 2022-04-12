f7987a056592babf1c3118e085fbd187b0c4eceb
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-04-12 11:22:13 +0200

    Split unit test to smaller projects
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

---
+++ tests/unit_tests/pal_log/src/main.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+#include <unity.h>
+#include <sid_pal_log_ifc.h>
+
+void setUp(void)
+{
+}
+
+/******************************************************************
+* sid_pal_log_ifc
+* ****************************************************************/
+
+void test_log_severity(void)
+{
+	sid_pal_log(SID_PAL_LOG_SEVERITY_ERROR, 1, "Sidewalk log Error");
+	sid_pal_log(SID_PAL_LOG_SEVERITY_WARNING, 1, "Sidewalk log Warning");
+	sid_pal_log(SID_PAL_LOG_SEVERITY_INFO, 1, "Sidewalk log Info");
+	sid_pal_log(SID_PAL_LOG_SEVERITY_DEBUG, 1, "Sidewalk log Debug");
+	sid_pal_log(SID_PAL_LOG_SEVERITY_DEBUG + 1, 1, "Sidewalk log Unknow");
+	TEST_PASS();
+}
+
+void test_log_arguments(void)
+{
+	sid_pal_log(SID_PAL_LOG_SEVERITY_INFO, 1, "%s", "Text");
+	sid_pal_log(SID_PAL_LOG_SEVERITY_INFO, 1, "%d", 1);
+	sid_pal_log(SID_PAL_LOG_SEVERITY_INFO, 5, "%d, %d, %d, %d, %d", 1, 2, 3, 4, 5);
+
+	sid_pal_log(SID_PAL_LOG_SEVERITY_INFO, 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	TEST_PASS();
+}
+
+void test_log_flush(void)
+{
+	sid_pal_log_flush();
+	TEST_PASS();
+}
+
+void test_log_push_string(void)
+{
+	char test_string[] = "test message 123";
+
+	const char *ret_string = sid_pal_log_push_str(test_string);
+
+	TEST_ASSERT_EQUAL_STRING(test_string, ret_string);
+}
+
+void test_log_get_buffer(void)
+{
+	bool ret;
+	struct sid_pal_log_buffer *const test_log_buffer = NULL;
+
+	ret = sid_pal_log_get_log_buffer(test_log_buffer);
+	TEST_ASSERT_FALSE_MESSAGE(ret, "sid_pal_log_get_log_buffer not implemented, should always return false.");
+}
+/* It is required to be added to each test. That is because unity is using
+ * different main signature (returns int) and zephyr expects main which does
+ * not return value.
+ */
+extern int unity_main(void);
+
+void main(void)
+{
+	(void)unity_main();
+}
---
+++ tests/unit_tests/pal_uptime/src/main.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+#include <unity.h>
+#include <sid_pal_uptime_ifc.h>
+#include <sys_clock.h>
+#include <mock_zephyr_time.h>
+
+void setUp(void)
+{
+}
+
+/******************************************************************
+* sid_pal_uptime_ifc
+* ****************************************************************/
+static void uptime_test_time(uint64_t uptime_nanoseconds)
+{
+	uint32_t seconds, nanoseconds;
+	struct sid_timespec sid_time;
+
+	seconds = (uint32_t)(uptime_nanoseconds / NSEC_PER_SEC);
+	nanoseconds = (uint32_t)(uptime_nanoseconds % NSEC_PER_SEC);
+
+	__wrap_zephyr_uptime_ns_ExpectAndReturn(uptime_nanoseconds);
+	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_uptime_now(&sid_time));
+
+	TEST_ASSERT_EQUAL(seconds, sid_time.tv_sec);
+	TEST_ASSERT_EQUAL(nanoseconds, sid_time.tv_nsec);
+}
+
+void test_sid_pal_uptime_get_now(void)
+{
+	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER, sid_pal_uptime_now(NULL));
+
+	uptime_test_time(0ull);
+	uptime_test_time(1ull * NSEC_PER_SEC + 10ull);
+	uptime_test_time(10ull * NSEC_PER_SEC + 100ull);
+	uptime_test_time(LONG_MAX);
+	uptime_test_time(LLONG_MAX);
+	uptime_test_time(ULLONG_MAX);
+}
+
+void test_sid_pal_uptime_accuracy(void)
+{
+	/* Note: This is a oversimplified test fo dummy implementation.*/
+	int16_t ppm;
+
+	ppm = sid_pal_uptime_get_xtal_ppm();
+	sid_pal_uptime_set_xtal_ppm(ppm);
+	TEST_ASSERT_EQUAL(ppm, sid_pal_uptime_get_xtal_ppm());
+}
+
+
+/* It is required to be added to each test. That is because unity is using
+ * different main signature (returns int) and zephyr expects main which does
+ * not return value.
+ */
+extern int unity_main(void);
+
+void main(void)
+{
+	(void)unity_main();
+}
