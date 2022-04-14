54408f054f8012a865aadb414f0ba6b9e64c1974
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-04-14 14:32:14 +0200

    Add source and map.yml file exist checking before test execution

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 #
 # Copyright (c) 2022 Nordic Semiconductor ASA
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
 #
 
-SIDEWALK_BASE=$ZEPHYR_BASE/../sidewalk
-TWISTER_BIN=$ZEPHYR_BASE/scripts/twister
-HARDWARE_MAP_FILE=$SIDEWALK_BASE/scripts/ci/map.yml
+SIDEWALK_BASE=${ZEPHYR_BASE}/../sidewalk
+TWISTER_BIN=${ZEPHYR_BASE}/scripts/twister
+HARDWARE_MAP_FILE=${SIDEWALK_BASE}/scripts/ci/map.yml
+
+# Treat unset variables as errors when performing parameter expansion
+set -o nounset -o errexit
+source ${ZEPHYR_BASE}/../zephyr/zephyr-env.sh
 
 echo "> Build samples"
 $TWISTER_BIN --verbose --platform nrf52840dk_nrf52840 --build-only --show-footprint --testcase-root $SIDEWALK_BASE
@@ -19,4 +24,10 @@ if [ -f "$HARDWARE_MAP_FILE" ]; then
 	$TWISTER_BIN --verbose --platform nrf52840dk_nrf52840 --testcase-root $SIDEWALK_BASE/tests/nrf_integration --device-testing --hardware-map $HARDWARE_MAP_FILE
 else
 	echo "Can't run integration tests. File: $HARDWARE_MAP_FILE does not exist."
+	echo "Follow the https://projecttools.nordicsemi.no/confluence/display/KRKNWK/SID2%3A+How+to+run+tests+with+Twister instruction"
+	echo "to prepare required map.yml file."
+	exit 1
 fi
+
+exit 0
+
