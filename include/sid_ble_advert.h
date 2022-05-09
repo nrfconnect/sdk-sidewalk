852f967e0c7c9f581bdeebd09fd094acfa5f1897
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-05-09 10:47:00 +0200

    Add advertising fuctions doc comments
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- include/sid_ble_advert.h
+++ include/sid_ble_advert.h
@@ -9,8 +9,32 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Start Bluetooth advertising.
+ *
+ * @return Zero on success or (negative) error code on failure.
+ */
 int sid_ble_advert_start(void);
+
+/**
+ * @brief Stop Bluetooth advertising.
+ *
+ * @return Zero on success or (negative) error code on failure.
+ */
 int sid_ble_advert_stop(void);
+
+/**
+ * @brief Update advertising data.
+ *
+ * @note update the value of manufacturing section in ble advertising.
+ * Data may be trimmed to meet bluetooth advertising size requirements.
+ * Too long manufacuring data may affect Device Name.
+ *
+ * @param data buffor of data to be updated.
+ * @param data_len length of data to be updated in bytes.
+ *
+ * @return Zero on success or (negative) error code on failure.
+ */
 int sid_ble_advert_update(uint8_t *data, uint8_t data_len);
 
 #endif /* NRF_BLE_ADVERT_H */
