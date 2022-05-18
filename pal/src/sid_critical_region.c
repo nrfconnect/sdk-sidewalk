beaf760e65d762148e026e584a4dad82416963b8
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-05-18 15:09:57 +0200

    Implement ciritcal redion with mutex
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- pal/src/sid_critical_region.c
+++ pal/src/sid_critical_region.c
@@ -10,10 +10,16 @@
 
 #include <sid_pal_critical_region_ifc.h>
 
-void sid_pal_enter_critical_region(){
-	// TODO: dummy implentation to at least build sample
+#include <kernel.h>
+
+K_MUTEX_DEFINE(sid_mutex);
+
+void sid_pal_enter_critical_region()
+{
+	k_mutex_lock(&sid_mutex, K_FOREVER);
 }
 
-void sid_pal_exit_critical_region() {
-	// TODO: dummy implentation to at least build sample
+void sid_pal_exit_critical_region()
+{
+	k_mutex_unlock(&sid_mutex);
 }
