e3df891b85bb7d14e61967e76c1d2c327cd25934
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-05-18 15:09:57 +0200

    Logs for ace utils
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/sid_ble/src/ace_utils.c
+++ samples/sid_ble/src/ace_utils.c
@@ -2,17 +2,23 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#include <logging/log.h>
+LOG_MODULE_REGISTER(ace, LOG_LEVEL_DBG);
+
 typedef int aceModules_moduleId_t;
 typedef int aceAlloc_bufferType_t;
 
 void *aceAlloc_alloc(aceModules_moduleId_t module_id,
 		     aceAlloc_bufferType_t buf_type, size_t size)
 {
-	return malloc(size);
+	void * p = malloc(size);
+	LOG_INF("Alloc: %p %d", p, size);
+	return p;
 }
 
 void aceAlloc_free(aceModules_moduleId_t module_id,
 		   aceAlloc_bufferType_t buf_type, void *p)
 {
+	LOG_INF("Free: %p", p);
 	return free(p);
 }
