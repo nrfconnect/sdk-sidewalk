a75493af1b3bf13e34786cf2b0aa822192941ce8
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-05-18 15:09:57 +0200

    Add dummy ciritcal region impl
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

---
+++ pal/src/sid_critical_region.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+/** @file sid_log.c
+ *  @brief Log interface implementation.
+ */
+
+#include <sid_pal_critical_region_ifc.h>
+
+void sid_pal_enter_critical_region(){
+	// TODO: dummy implentation to at least build sample
+}
+
+void sid_pal_exit_critical_region() {
+	// TODO: dummy implentation to at least build sample
+}
