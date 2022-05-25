ddb7af57e87b74de1f2a1788582f6f5e4d54a50c
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-05-25 10:31:29 +0200

    Add empty sidewalk temaplate

---
+++ samples/sid_template/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+#include <logging/log.h>
+#include <zephyr.h>
+
+LOG_MODULE_REGISTER(sid_template, CONFIG_SIDEWALK_LOG_LEVEL);
+
+void main(void)
+{
+	for (;;) {
+
+	}
+}
