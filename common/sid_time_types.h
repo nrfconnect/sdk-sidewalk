9fb79321bdde13038f9a1398441f8cf6f2baef3e
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-06-24 22:05:16 +0200

    Add missing copyright & license text

--- common/sid_time_types.h
+++ common/sid_time_types.h
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All rights reserved.
+ *
+ * AMAZON PROPRIETARY/CONFIDENTIAL
+ *
+ * You may not use this file except in compliance with the terms and
+ * conditions set forth in the accompanying LICENSE.txt file.
+ *
+ * THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY
+ * DISCLAIMS, WITH RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS,
+ * IMPLIED, OR STATUTORY, INCLUDING THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+ */
+
 #ifndef SID_TIME_TYPES_H
 #define SID_TIME_TYPES_H
 
