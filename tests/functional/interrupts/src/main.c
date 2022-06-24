481ec75a591211427153ba404f7f1080e5c2650d
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2022-06-24 17:55:02 +0200

    test: improve swi test
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- tests/functional/interrupts/src/main.c
+++ tests/functional/interrupts/src/main.c
@@ -6,25 +6,32 @@
 #include <unity.h>
 #include <sid_error.h>
 #include <sid_pal_swi_ifc.h>
+#include <string.h>
 
-#define UNCHANGED 0
-#define CHANGED 1
-static volatile int resource;
+#define BUFFER_OUT_NO 100
+#define BUFFER_SIZE 8
+static char buffer_in[BUFFER_SIZE] = "test_me";
+static char buffer_out[BUFFER_OUT_NO][BUFFER_SIZE];
 
 static void swi_callback(void)
 {
-	resource = CHANGED;
+	static size_t buf_out_cnt = 0;
+
+	(void)memcpy(buffer_out[buf_out_cnt++], buffer_in, BUFFER_SIZE);
 }
 
 void test_sid_pal_swi(void)
 {
 	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_swi_init(swi_callback));
 
-	resource = UNCHANGED;
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_swi_trigger());
-	TEST_ASSERT_EQUAL(CHANGED, resource);
-}
+	for (int i = 0; i < BUFFER_OUT_NO; i++) {
+		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_swi_trigger());
+	}
 
+	for (int i = 0; i < BUFFER_OUT_NO; i++) {
+		TEST_ASSERT_EQUAL_UINT8_ARRAY(buffer_in, buffer_out[i], BUFFER_SIZE);
+	}
+}
 
 /* It is required to be added to each test. That is because unity is using
  * different main signature (returns int) and zephyr expects main which does
