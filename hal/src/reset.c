f1ace83fe6be679496f8cc816b63f58a2e1e5cd4
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2022-07-05 08:16:54 +0200

    Sidewalk HAL implementation

---
+++ hal/src/reset.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+/** @file reset.c
+ *  @brief HAL reboot (reset) implementation.
+ */
+
+#include <sid_hal_reset_ifc.h>
+#include <sys/reboot.h>
+#include <zephyr.h>
+
+sid_error_t sid_hal_reset(sid_hal_reset_type_t type)
+{
+	if (SID_HAL_RESET_NORMAL == type) {
+		sys_reboot(SYS_REBOOT_WARM);
+	} else {
+		return SID_ERROR_NOSUPPORT;
+	}
+
+	return SID_ERROR_NONE;
+}
