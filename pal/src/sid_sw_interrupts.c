abc5b6e854cf82fbe5f060eec2f6048e9912060f
Robert Gałat <robert.galat@nordicsemi.no>
2022-07-12 14:07:53 +0200

    SWI: schedule software interrupts in workq
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- pal/src/sid_sw_interrupts.c
+++ pal/src/sid_sw_interrupts.c
@@ -11,28 +11,33 @@
 #include <sid_pal_swi_ifc.h>
 #include <kernel.h>
 
+static struct k_work swi_work;
 static sid_pal_swi_cb_t swi_cb;
+
+static void swi_work_fn(struct k_work *item)
+{
+	ARG_UNUSED(item);
+
+	if (swi_cb) {
+		swi_cb();
+	}
+}
 
 sid_error_t sid_pal_swi_init(sid_pal_swi_cb_t event_callback)
 {
 	if (!event_callback) {
 		return SID_ERROR_NULL_POINTER;
 	}
-
 	swi_cb = event_callback;
+
+	k_work_init(&swi_work, swi_work_fn);
 
 	return SID_ERROR_NONE;
 }
 
 sid_error_t sid_pal_swi_trigger(void)
 {
-	k_sched_lock();
-
-	if (swi_cb) {
-		swi_cb();
-	}
-
-	k_sched_unlock();
+	k_work_submit(&swi_work);
 
 	return SID_ERROR_NONE;
 }
