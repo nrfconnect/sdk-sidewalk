d7d5ae9d16391cf57d7f83a8e98745988493a348
Robert Gałat <robert.galat@nordicsemi.no>
2022-07-25 22:37:47 +0200

    workq: add gpio irq to workq
    move execution from IRQ to worker thread
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- pal/src/sid_gpio_irq_handler.c
+++ pal/src/sid_gpio_irq_handler.c
@@ -15,10 +15,22 @@
 typedef struct {
 	sid_pal_gpio_irq_handler_t handler;
 	void *args;
+	struct k_work work;
 } sid_gpio_irq_cb_t;
 
 static sid_gpio_irq_cb_t gpio_irq_cb[MAX_NUMBERS_OF_PINS];
 
+static void gpio_work_handler(struct k_work *w)
+{
+	sid_gpio_irq_cb_t *irq_cb = CONTAINER_OF(w, sid_gpio_irq_cb_t, work);
+	uint32_t number = ((size_t)irq_cb - (size_t)gpio_irq_cb) / sizeof(sid_gpio_irq_cb_t);
+
+	if (irq_cb->handler) {
+		irq_cb->handler(number, irq_cb->args);
+	}
+}
+
+// called from irq
 void sid_gpio_irq_callback(const struct device *gpiob,
 			   struct gpio_callback *cb,
 			   uint32_t pins)
@@ -36,7 +48,7 @@ void sid_gpio_irq_callback(const struct device *gpiob,
 	}
 
 	if (gpio_irq_cb[gpio_number].handler) {
-		gpio_irq_cb[gpio_number].handler(gpio_number, gpio_irq_cb[gpio_number].args);
+		k_work_submit(&gpio_irq_cb[gpio_number].work);
 	}
 }
 
@@ -51,5 +63,6 @@ void sid_gpio_irq_handler_set(uint32_t gpio_number,
 	if (gpio_irq_handler) {
 		gpio_irq_cb[gpio_number].handler = gpio_irq_handler;
 		gpio_irq_cb[gpio_number].args = callback_arg;
+		k_work_init(&gpio_irq_cb[gpio_number].work, gpio_work_handler);
 	}
 }
