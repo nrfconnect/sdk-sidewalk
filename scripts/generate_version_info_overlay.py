e0059cf119506444eec37866fa772747d33ac5ef
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-01-05 13:20:38 +0100

    scripts: fix version info on tags
    match tag version with regex
    return zeros when not found
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- scripts/generate_version_info_overlay.py
+++ scripts/generate_version_info_overlay.py
@@ -1,7 +1,13 @@
 import git
 from pathlib import Path
 import os
+import re
+
+def subtext(text, regex, if_none):
+    exp = re.search(regex, text)
+    return if_none if exp is None else exp.group(1)
 
 version = git.repo.Repo(Path(os.path.normpath(__file__)).parents[2] / "sidewalk").git.describe()
-version_numbers = "+".join(version[1:].split("-")[:-1])
+version_numbers = subtext(version, "v(\d+\.\d+\.\d+)", "0.0.0") + \
+    "+" + subtext(version, "v[\d\.]+-(\d+)", "0")
 print(f"CONFIG_MCUBOOT_IMAGE_VERSION=\"{version_numbers}\"")
