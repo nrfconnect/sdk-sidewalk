2f857f0ea4abd5f6e712c70cdeac731bd5a3ba87
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-01-09 17:43:17 +0100

    pal: ignore bt disable not supported error
    SoftDevice controller is unable to disable bluetooth, causing errors
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- pal/src/sid_ble_adapter.c
+++ pal/src/sid_ble_adapter.c
@@ -239,7 +239,7 @@ static sid_error_t ble_adapter_deinit(void)
 {
 	int err = bt_disable();
 
-	if (err) {
+	if (err < 0 && -ENOTSUP != err) {
 		LOG_ERR("BT disable failed (err %d)", err);
 	}
 	sid_ble_conn_deinit();
