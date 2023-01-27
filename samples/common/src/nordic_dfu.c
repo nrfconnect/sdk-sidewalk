f719756ddef4c2c69a3d0909f6f530e0a32a8e8f
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-01-27 11:35:04 +0100

    samples: nordic dfu over ble without ble callbacks
    [KRKNWK-16119]
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/common/src/nordic_dfu.c
+++ samples/common/src/nordic_dfu.c
@@ -6,26 +6,14 @@
  */
 
 #include "nordic_dfu.h"
-#include <zephyr/mgmt/mcumgr/smp_bt.h>
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/mgmt/mcumgr/smp_bt.h>
 #include "os_mgmt/os_mgmt.h"
 #include "img_mgmt/img_mgmt.h"
 
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(smp_bt_sample);
-
-static void connected(struct bt_conn *conn, uint8_t err);
-static void disconnected(struct bt_conn *conn, uint8_t reason);
-
-static struct bt_conn_cb conn_callbacks = {
-	.connected = connected,
-	.disconnected = disconnected,
-};
-
-static struct k_work advertise_work;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -34,48 +22,22 @@ static const struct bt_data ad[] = {
 		      0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
 };
 
-static void advertise(struct k_work *work)
+static void bt_ready(int bt_err_code)
 {
-	int rc;
-
-	bt_le_adv_stop();
-
-	rc = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (rc) {
-		LOG_ERR("Advertising failed to start (rc %d)", rc);
-		return;
-	}
-
-	LOG_INF("Advertising successfully started");
-}
-
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err) {
-		LOG_ERR("Connection failed (err 0x%02x)", err);
-	} else {
-		LOG_INF("Connected");
-	}
-}
-
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{
-	LOG_INF("Disconnected (reason 0x%02x)", reason);
-	k_work_submit(&advertise_work);
-}
-
-static void bt_ready(int err)
-{
-	if (err) {
-		LOG_ERR("Bluetooth init failed (err %d)", err);
+	if (bt_err_code) {
+		LOG_ERR("Bluetooth init failed (err %d)", bt_err_code);
 		return;
 	}
 
 	LOG_INF("Bluetooth initialized");
 
-	k_work_submit(&advertise_work);
+	int err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+		return;
+	}
 
-	bt_conn_cb_register(&conn_callbacks);
+	LOG_INF("Advertising successfully started");
 }
 
 int nordic_dfu_ble_start(void)
@@ -84,20 +46,18 @@ int nordic_dfu_ble_start(void)
 	os_mgmt_register_group();
 	img_mgmt_register_group();
 
-	k_work_init(&advertise_work, advertise);
-
 	/* Enable Bluetooth. */
-	int rc = bt_enable(bt_ready);
+	int err = bt_enable(bt_ready);
 
-	switch (rc) {
+	switch (err) {
 	case -EALREADY:
 		LOG_INF("Bluetooth already initialized");
 		bt_ready(0);
 	case 0:
 		break;
 	default:
-		LOG_ERR("Bluetooth init failed (err %d)", rc);
-		return rc;
+		LOG_ERR("Bluetooth enable failed (err %d)", err);
+		return err;
 	}
 
 	/* Initialize the Bluetooth mcumgr transport. */
