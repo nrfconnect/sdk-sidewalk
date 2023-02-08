16a76fc13628e1d33eb8d353339592d3ba5795e0
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2023-02-08 07:58:05 +0100

    Code improvement: cast to proper types.
    The change in sid_uptime.c is related to unintentional integer overflow.
    Second change in the sid_log.c file is necessary because SID_PAL_LOG_LEVEL
    is taken from a cmake file, this type is incompatible and it needs to be cast.
    
    Signed-off-by: Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>

--- pal/src/sid_log.c
+++ pal/src/sid_log.c
@@ -82,6 +82,7 @@ bool sid_pal_log_get_log_buffer(struct sid_pal_log_buffer *const log_buffer)
 	return false;
 }
 
-sid_pal_log_severity_t sid_log_control_get_current_log_level(void){
-	return SID_PAL_LOG_LEVEL;
+sid_pal_log_severity_t sid_log_control_get_current_log_level(void)
+{
+	return (sid_pal_log_severity_t)SID_PAL_LOG_LEVEL;
 }
