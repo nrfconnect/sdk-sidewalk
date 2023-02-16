14d4a74a0805f490af951f8f98bd7dd4f25a9f86
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2023-02-16 10:05:04 +0100

    pal: Fix for re-entry to critical section.
    Signed-off-by: Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>

--- pal/src/sid_critical_region.c
+++ pal/src/sid_critical_region.c
@@ -9,17 +9,33 @@
  */
 
 #include <sid_pal_critical_region_ifc.h>
+#include <assert.h>
 
 #include <zephyr/kernel.h>
 
-static unsigned int key;
+#define RE_ENTRY_MAX_AMOUNT     (8)
+
+static atomic_t count = ATOMIC_INIT(0);
+static unsigned int key = 0;
 
 void sid_pal_enter_critical_region()
 {
-	key = irq_lock();
+	const unsigned int prev_val = atomic_add(&count, 1);
+
+	if (prev_val == 0) {
+		key = irq_lock();
+	}
+
+	assert(prev_val <= RE_ENTRY_MAX_AMOUNT);
 }
 
 void sid_pal_exit_critical_region()
 {
-	irq_unlock(key);
+	const unsigned int prev_val = atomic_sub(&count, 1);
+
+	assert(prev_val > 0);
+
+	if (prev_val == 1) {
+		irq_unlock(key);
+	}
 }
