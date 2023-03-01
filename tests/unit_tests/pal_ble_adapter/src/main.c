02f90439b2e8d97ca46ea1756dba5c82e101242d
Robert Gałat <robert.galat@nordicsemi.no>
2023-03-01 14:07:54 +0000

    ble: add mtu negotiation in connection handler
    Some applications that connect to the device initiate MTU negotiation,
    and some don't in case where no one initiate the mtu change,
    it is left at minimum value.
    This causes increased number of messages, and delay in sending
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- tests/unit_tests/pal_ble_adapter/src/main.c
+++ tests/unit_tests/pal_ble_adapter/src/main.c
@@ -27,6 +27,7 @@ FAKE_VALUE_FUNC(int, bt_enable, bt_ready_cb_t);
 FAKE_VALUE_FUNC(int, bt_disable);
 FAKE_VALUE_FUNC(int, bt_le_adv_start, const struct bt_le_adv_param *, const struct bt_data *, size_t,
 		const struct bt_data *, size_t);
+FAKE_VALUE_FUNC(int, bt_gatt_exchange_mtu, struct bt_conn *, struct bt_gatt_exchange_params *);
 
 FAKE_VALUE_FUNC(int, bt_le_adv_stop);
 FAKE_VOID_FUNC(bt_conn_cb_register, struct bt_conn_cb *);
@@ -48,9 +49,13 @@ FAKE_VALUE_FUNC(ssize_t, bt_gatt_attr_write_ccc, struct bt_conn *,
 
 #define FFF_FAKES_LIST(FAKE)		\
 	FAKE(bt_enable)			\
+	FAKE(bt_disable)                \
 	FAKE(bt_le_adv_start)		\
+	FAKE(bt_le_adv_stop)            \
+	FAKE(bt_gatt_exchange_mtu)      \
 	FAKE(bt_conn_cb_register)	\
 	FAKE(bt_conn_ref)		\
+	FAKE(bt_conn_unref)             \
 	FAKE(bt_conn_get_dst)		\
 	FAKE(bt_gatt_attr_read_service)	\
 	FAKE(bt_gatt_attr_read_chrc)	\
