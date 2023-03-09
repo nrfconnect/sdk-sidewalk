8646dc1f5036179b99948a741339e33829175df9
Robert Gałat <robert.galat@nordicsemi.no>
2023-03-09 11:55:10 +0000

    refactor: move common configuration to subsys
    KRKNWK-16201
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- samples/common/src/sidewalk_thread.c
+++ samples/common/src/sidewalk_thread.c
@@ -15,19 +15,24 @@
 #include <sid_pal_mfg_store_ifc.h>
 #include <sid_hal_reset_ifc.h>
 #include <sid_pal_assert_ifc.h>
-#if defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
+#if defined(CONFIG_SIDEWALK_SUBGHZ)
 #include <sid_900_cfg.h>
 #include <sx126x_config.h>
 #include <sid_pal_serial_bus_ifc.h>
 #include <sid_pal_serial_bus_spi_config.h>
-#endif /* defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#endif /* defined(CONFIG_SIDEWALK_SUBGHZ) */
+
+#include <app_ble_config.h>
+#if defined(CONFIG_SIDEWALK_SUBGHZ)
+#include <app_subGHz_config.h>
+#endif
 
 #include <zephyr/kernel.h>
 #include <zephyr/storage/flash_map.h>
 #include <dk_buttons_and_leds.h>
 #include <sid_pal_gpio_ifc.h>
 #include "nordic_dfu.h"
-
+#include <pal_init.h>
 #ifdef CONFIG_SIDEWALK_CLI
 #include <sid_shell.h>
 #endif
@@ -124,125 +129,7 @@ static struct sid_device_profile profile_from_dev = {
 };
 #endif
 
-#if defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
-#define REGION_US915
 
-/* This product has no external PA and SX1262 can support max of 22dBm*/
-#define RADIO_SX1262_MAX_TX_POWER                                  22
-#define RADIO_SX1262_MIN_TX_POWER                                  -9
-
-#define RADIO_MAX_TX_POWER_NA                                      20
-#define RADIO_MAX_TX_POWER_EU                                      14
-
-#if defined (REGION_ALL)
-#define RADIO_REGION                                               RADIO_REGION_NONE
-#elif defined (REGION_US915)
-#define RADIO_REGION                                               RADIO_REGION_NA
-#elif defined (REGION_EU868)
-#define RADIO_REGION                                               RADIO_REGION_EU
-#endif
-
-#define RADIO_SX1262_SPI_BUFFER_SIZE                               255
-
-#define RADIO_SX1262_PA_DUTY_CYCLE                                 0x04
-#define RADIO_SX1262_HP_MAX                                        0x07
-#define RADIO_SX1262_DEVICE_SEL                                    0x00
-#define RADIO_SX1262_PA_LUT                                        0x01
-
-#define RADIO_RX_LNA_GAIN                                          0
-#define RADIO_MAX_CAD_SYMBOL                                       SID_PAL_RADIO_LORA_CAD_04_SYMBOL
-#define RADIO_ANT_GAIN(X)                                          ((X) * 100)
-
-static const struct sid_pal_serial_bus_factory radio_spi_factory = {
-	.create = sid_pal_serial_bus_nordic_spi_create,
-};
-
-static uint8_t radio_sx1262_buffer[RADIO_SX1262_SPI_BUFFER_SIZE] = { 0 };
-
-static int32_t radio_sx1262_pa_cfg(int8_t tx_power, radio_sx126x_pa_cfg_t *pa_cfg)
-{
-	int8_t pwr = tx_power;
-
-	if (tx_power > RADIO_SX1262_MAX_TX_POWER) {
-		pwr = RADIO_SX1262_MAX_TX_POWER;
-	}
-
-	if (tx_power < RADIO_SX1262_MIN_TX_POWER) {
-		pwr = RADIO_SX1262_MIN_TX_POWER;
-	}
-
-	pa_cfg->pa_duty_cycle = RADIO_SX1262_PA_DUTY_CYCLE;
-	pa_cfg->hp_max = RADIO_SX1262_HP_MAX;
-	pa_cfg->device_sel = RADIO_SX1262_DEVICE_SEL;
-	pa_cfg->pa_lut = RADIO_SX1262_PA_LUT;
-	pa_cfg->tx_power = pwr;  // one to one mapping between tx params and tx power
-	pa_cfg->ramp_time = RADIO_SX126X_RAMP_40_US;
-
-	return 0;
-}
-
-const radio_sx126x_regional_param_t radio_sx126x_regional_param[] =
-{
-    #if defined (REGION_ALL) || defined (REGION_US915)
-	{
-		.param_region = RADIO_REGION_NA,
-		.max_tx_power = { RADIO_MAX_TX_POWER_NA, RADIO_MAX_TX_POWER_NA, RADIO_MAX_TX_POWER_NA,
-				  RADIO_MAX_TX_POWER_NA, RADIO_MAX_TX_POWER_NA, RADIO_MAX_TX_POWER_NA },
-		.cca_level_adjust = { 0, 0, 0, 0, 0, 0 },
-		.ant_dbi = RADIO_ANT_GAIN(2.15)
-	},
-    #endif
-    #if defined (REGION_ALL) || defined (REGION_EU868)
-	{
-		.param_region = RADIO_REGION_EU,
-		.max_tx_power = { RADIO_MAX_TX_POWER_EU, RADIO_MAX_TX_POWER_EU, RADIO_MAX_TX_POWER_EU,
-				  RADIO_MAX_TX_POWER_EU, RADIO_MAX_TX_POWER_EU, RADIO_MAX_TX_POWER_EU },
-		.cca_level_adjust = { 0, 0, 0, 0, 0, 0 },
-		.ant_dbi = RADIO_ANT_GAIN(2.15)
-	},
-    #endif
-};
-
-const radio_sx126x_device_config_t radio_sx1262_cfg = {
-	.id = SEMTECH_ID_SX1262,                     // chip id register not supported
-	.regulator_mode = RADIO_SX126X_REGULATOR_DCDC,
-	.rx_boost = false,
-	.lna_gain = RADIO_RX_LNA_GAIN,
-	.bus_factory = &radio_spi_factory,
-	.gpio_power = 3,                // sx1262_NRESET
-	.gpio_int1 = 38,                // sx1262_DIO1
-	.gpio_radio_busy = 36,          // sx1262_BUSY
-	.gpio_rf_sw_ena = 42,           // sx1262 ANT_SW
-	.gpio_tx_bypass = 128,
-	.bus_selector = {
-		.client_selector = 40, // sx1262_NSS
-		.bit_order = SID_PAL_SERIAL_BUS_BIT_ORDER_MSB_FIRST,
-	},
-	.pa_cfg_callback = radio_sx1262_pa_cfg,
-
-	.tcxo = {
-		.ctrl = SX126X_TCXO_CTRL_NONE,
-	},
-
-	.regional_config = {
-		.radio_region = RADIO_REGION,
-		.reg_param_table_size = sizeof(radio_sx126x_regional_param) / sizeof(radio_sx126x_regional_param[0]),
-		.reg_param_table = radio_sx126x_regional_param,
-	},
-
-	.state_timings = {
-		.sleep_to_full_power_us = 440,
-		.full_power_to_sleep_us = 0,
-		.rx_to_tx_us = 0,
-		.tx_to_rx_us = 0,
-	},
-
-	.internal_buffer = {
-		.p = radio_sx1262_buffer,
-		.size = sizeof(radio_sx1262_buffer),
-	},
-};
-#endif /* defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
 
 static void on_sidewalk_event(bool in_isr, void *context)
 {
@@ -361,9 +248,9 @@ static void on_sidewalk_status_changed(const struct sid_status *status, void *co
 	case SID_STATE_READY:
 		app_ctx->state = STATE_SIDEWALK_READY;
 		application_state_connected(&global_state_notifier, true);
-#if !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
+#if !defined(CONFIG_SIDEWALK_SUBGHZ)
 		app_ctx->connection_request = false;
-#endif /* !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#endif /* !defined(CONFIG_SIDEWALK_SUBGHZ) */
 		break;
 	case SID_STATE_NOT_READY:
 		app_ctx->state = STATE_SIDEWALK_NOT_READY;
@@ -420,7 +307,7 @@ static void on_sidewalk_factory_reset(void *context)
 	}
 }
 
-#if !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
+#if !defined(CONFIG_SIDEWALK_SUBGHZ)
 static void connection_request(app_context_t *app_ctx)
 {
 	if (STATE_SIDEWALK_READY == app_ctx->state) {
@@ -437,7 +324,7 @@ static void connection_request(app_context_t *app_ctx)
 	}
 }
 
-#else /* !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#else /* !defined(CONFIG_SIDEWALK_SUBGHZ) */
 static void device_profile_get(app_context_t *app_ctx)
 {
 	sid_error_t ret = sid_option(app_ctx->sidewalk_handle, SID_OPTION_900MHZ_GET_DEVICE_PROFILE,
@@ -472,7 +359,7 @@ static void device_profile_set(app_context_t *app_ctx)
 
 }
 
-#endif /* !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#endif /* !defined(CONFIG_SIDEWALK_SUBGHZ) */
 
 static void factory_reset(app_context_t *app_ctx)
 {
@@ -551,60 +438,8 @@ static void sid_dfu_switch_state(app_context_t *app_ctx)
 
 #endif /* CONFIG_SIDEWALK_DFU_SERVICE_BLE */
 
-static sid_error_t sid_pal_init(void)
-{
-	sid_error_t ret_code;
-
-	ret_code = sid_pal_storage_kv_init();
-	if (SID_ERROR_NONE != ret_code) {
-		LOG_ERR("Sidewalk KV store init failed, err: %d", ret_code);
-		return ret_code;
-	}
-
-	ret_code = sid_pal_crypto_init();
-	if (SID_ERROR_NONE != ret_code) {
-		LOG_ERR("Sidewalk Init Crypto HAL, err: %d", ret_code);
-		return ret_code;
-	}
-
-	static const sid_pal_mfg_store_region_t mfg_store_region = {
-		.addr_start = (uintptr_t)(FLASH_AREA_OFFSET(mfg_storage)),
-		.addr_end = (uintptr_t)(FLASH_AREA_OFFSET(mfg_storage) + FLASH_AREA_SIZE(mfg_storage)),
-	};
-
-	sid_pal_mfg_store_init(mfg_store_region);
-
-	if (sid_pal_mfg_store_get_version() == EMPTY_MFG_HEX_PARTITION) {
-		LOG_ERR("The mfg.hex version mismatch");
-		LOG_ERR("Check if the file has been generated and flashed properly");
-		LOG_ERR("START ADDRESS: 0x%08x", FLASH_AREA_OFFSET(mfg_storage));
-		LOG_ERR("SIZE: 0x%08x", FLASH_AREA_SIZE(mfg_storage));
-		return SID_ERROR_NOT_FOUND;
-	}
-
-#if defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
-	set_radio_sx126x_device_config(&radio_sx1262_cfg);
-#endif /* defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
-
-	return SID_ERROR_NONE;
-}
-
 static sid_error_t sid_lib_run(app_context_t *app_ctx)
 {
-	static const sid_ble_link_config_t ble_link_config = {
-		.create_ble_adapter = sid_pal_ble_adapter_create,
-		.config = NULL,
-	};
-
-#if defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
-	static const struct sid_sub_ghz_links_config sub_ghz_link_config = {
-		.enable_link_metrics = true,
-		.registration_config = {
-			.enable = true,
-			.periodicity_s = UINT32_MAX,
-		},
-	};
-#endif /* defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
 
 	static struct sid_event_callbacks event_callbacks = {
 		.context = NULL,
@@ -619,13 +454,13 @@ static sid_error_t sid_lib_run(app_context_t *app_ctx)
 	event_callbacks.context = app_ctx;
 	app_ctx->sidewalk_config.link_mask = 0;
 	app_ctx->sidewalk_config.callbacks = &event_callbacks;
-	app_ctx->sidewalk_config.link_config = &ble_link_config;
+	app_ctx->sidewalk_config.link_config = app_get_ble_config();
 	app_ctx->sidewalk_config.time_sync_periodicity_seconds = 7200;
-#if defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
-	app_ctx->sidewalk_config.sub_ghz_link_config = &sub_ghz_link_config;
+#if defined(CONFIG_SIDEWALK_SUBGHZ)
+	app_ctx->sidewalk_config.sub_ghz_link_config = app_get_sub_ghz_config();
 #else
 	app_ctx->sidewalk_config.sub_ghz_link_config = NULL;
-#endif /* defined(CONFIG_SIDEWALK_LINK_MASK_FSK) || defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#endif /* defined(CONFIG_SIDEWALK_SUBGHZ) */
 
 	LOG_INF("Initializing sidewalk, built-in %s link mask", LM_2_STR(LINK_MASK));
 
@@ -639,9 +474,9 @@ static sid_error_t sid_lib_run(app_context_t *app_ctx)
 
 	app_ctx->state = STATE_SIDEWALK_NOT_READY;
 	application_state_connected(&global_state_notifier, false);
-#if !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA)
+#if !defined(CONFIG_SIDEWALK_SUBGHZ)
 	app_ctx->connection_request = false;
-#endif /* !defined(CONFIG_SIDEWALK_LINK_MASK_FSK) && !defined(CONFIG_SIDEWALK_LINK_MASK_LORA) */
+#endif /* !defined(CONFIG_SIDEWALK_SUBGHZ) */
 	return SID_ERROR_NONE;
 }
 
@@ -657,7 +492,7 @@ static void sidewalk_thread(void *context, void *u2, void *u3)
 	state_watch_init_log(&global_state_notifier);
 	#endif
 
-	if (SID_ERROR_NONE != sid_pal_init()) {
+	if (SID_ERROR_NONE != application_pal_init()) {
 		app_ctx->state = STATE_PAL_INIT_ERROR;
 		application_state_error(&global_state_notifier, true);
 		return;
