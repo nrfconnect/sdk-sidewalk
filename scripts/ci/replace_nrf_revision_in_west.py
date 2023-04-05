c21a02ab9d9c4b5ae6090012ade220874ef18d91
Robert Gałat <robert.galat@nordicsemi.no>
2023-04-05 15:13:41 +0000

    github: add nightly compatiblity check with nrf
    at 1:00 am every day build sidewalk samples and tests using
    the latest avaliable version of nrf
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

---
+++ scripts/ci/replace_nrf_revision_in_west.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import yaml
+import sys
+
+manifest = dict()
+with open(sys.argv[1], "r") as f:
+    manifest = yaml.load(f)
+
+nrf = filter(lambda a: a["name"] == "nrf", manifest["manifest"]["projects"])
+# I expect that there will be 1 such element, if not exeption will be thrown by next()
+next(nrf)["revision"] = "main"
+
+with open(sys.argv[1], "w") as f:
+    yaml.dump(manifest, f)
