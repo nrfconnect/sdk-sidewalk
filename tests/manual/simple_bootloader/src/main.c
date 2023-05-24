09528a4a3885bbd3eb09623551a18f9b53547263
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-05-24 11:30:57 +0200

    tests: Turn on led in simple bootloader app
    [KRKNWK-12220]
    * Turn on LED when app started
    * Fix code style error
    * Update lincense year
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- tests/manual/simple_bootloader/src/main.c
+++ tests/manual/simple_bootloader/src/main.c
@@ -6,12 +6,13 @@
 
 #include <zephyr/dfu/mcuboot.h>
 
+#include <dk_buttons_and_leds.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 static void app_setup(void)
 {
-	#if defined(CONFIG_BOOTLOADER_MCUBOOT)
+#if defined(CONFIG_BOOTLOADER_MCUBOOT)
 	if (!boot_is_img_confirmed()) {
 		int ret = boot_write_img_confirmed();
 
@@ -21,7 +22,9 @@ static void app_setup(void)
 			LOG_INF("Marked image as OK");
 		}
 	}
-	#endif
+#endif
+
+	dk_leds_init();
 }
 
 int main(void)
@@ -29,6 +32,7 @@ int main(void)
 	app_setup();
 
 	LOG_INF("Hello world!");
+	dk_set_led_on(DK_LED2);
 
 	return 0;
 }
