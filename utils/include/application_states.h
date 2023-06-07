33f733ef2d0084c8aeff10f037ae20e44803755f
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-06-07 11:41:45 +0200

    utils: application states gpio per platform
    [KRKNWK-16967]
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- utils/include/application_states.h
+++ utils/include/application_states.h
@@ -6,8 +6,10 @@
 
 #ifndef APPLICATION_STATES_H
 #define APPLICATION_STATES_H
+#include <zephyr/kernel.h>
 
 /* X(state_name, gpio_port, gpio_pin) */
+#if defined(NRF52840_XXAA)
 #define X_APPLICAITON_STATES                                                                       \
 	X(error, gpio1, 7)                                                                         \
 	X(working, gpio1, 5)                                                                       \
@@ -18,5 +20,19 @@
 	X(dfu, gpio1, 1)                                                                           \
 	X(sending, gpio1, 3)                                                                       \
 	X(receiving, gpio1, 2)
+#elif defined(NRF5340_XXAA)
+#define X_APPLICAITON_STATES                                                                       \
+	X(error, gpio1, 8)                                                                         \
+	X(working, gpio1, 6)                                                                       \
+	X(registered, gpio0, 28)                                                                   \
+	X(time_sync, gpio0, 29)                                                                    \
+	X(link, gpio0, 30)                                                                         \
+	X(connected, gpio0, 31)                                                                    \
+	X(dfu, gpio1, 0)                                                                           \
+	X(sending, gpio1, 4)                                                                       \
+	X(receiving, gpio1, 1)
+#else
+#error "Unknow device application states."
+#endif
 
 #endif /* APPLICATION_STATES_H */
