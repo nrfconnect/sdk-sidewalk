f1c18dbe2faec17608ac25452d88f4035d99ff12
Robert Gałat <robert.galat@nordicsemi.no>
2023-07-11 11:50:08 +0000

    tests: port functional/spi_bus to ztest
    port test to ztest framework
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- tests/functional/spi_bus/src/main.c
+++ tests/functional/spi_bus/src/main.c
@@ -4,11 +4,12 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <zephyr/ztest.h>
+
 #include <zephyr/kernel.h>
 #include <stdint.h>
 #include <string.h>
 
-#include <unity.h>
 #include <stdlib.h>
 
 #include <sid_pal_serial_bus_ifc.h>
@@ -20,100 +21,100 @@
 struct sid_pal_serial_bus_client client =
 	(struct sid_pal_serial_bus_client){ .client_selector = NSS_PIN_NUMBER };
 
-void test_init_spi()
+ZTEST(spi_bus, test_init_spi)
 {
 	const struct sid_pal_serial_bus_iface *interface;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
 }
 
-void test_invalid_init_spi()
+ZTEST(spi_bus, test_invalid_init_spi)
 {
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, sid_pal_serial_bus_nordic_spi_create(NULL, NULL));
+	zassert_equal(SID_ERROR_INVALID_ARGS, sid_pal_serial_bus_nordic_spi_create(NULL, NULL));
 }
 
-void test_xfer_no_buffer()
+ZTEST(spi_bus, test_xfer_no_buffer)
 {
 	const struct sid_pal_serial_bus_iface *interface = NULL;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
-	TEST_ASSERT(interface);
-	TEST_ASSERT(interface->xfer);
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_not_null(interface);
+	zassert_not_null(interface->xfer);
 	sid_error_t e = interface->xfer(interface, NULL, NULL, NULL, 1);
 
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, e);
+	zassert_equal(SID_ERROR_INVALID_ARGS, e);
 }
 
-void test_xfer_no_interface()
+ZTEST(spi_bus, test_xfer_no_interface)
 {
 	const struct sid_pal_serial_bus_iface *interface = NULL;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
-	TEST_ASSERT(interface);
-	TEST_ASSERT(interface->xfer);
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_not_null(interface);
+	zassert_not_null(interface->xfer);
 	uint8_t tx[] = { 0x1d, 0x08, 0xac, 0, 0 };
 	uint8_t rx[5] = { 0 };
 	sid_error_t e = interface->xfer(NULL, NULL, tx, rx, sizeof(rx));
 
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, e);
+	zassert_equal(SID_ERROR_INVALID_ARGS, e);
 	uint8_t rx_expect[5] = { 0 };
 
-	TEST_ASSERT_EQUAL_MEMORY(rx_expect, rx, sizeof(rx));
+	zassert_mem_equal(rx_expect, rx, sizeof(rx));
 }
 
-void test_xfer_wrong_interface()
+ZTEST(spi_bus, test_xfer_wrong_interface)
 {
 	const struct sid_pal_serial_bus_iface *interface = NULL;
 	const struct sid_pal_serial_bus_iface interface2;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
-	TEST_ASSERT(interface);
-	TEST_ASSERT(interface->xfer);
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_not_null(interface);
+	zassert_not_null(interface->xfer);
 	uint8_t tx[] = { 0x1d, 0x08, 0xac, 0, 0 };
 	uint8_t rx[5] = { 0 };
 	sid_error_t e = interface->xfer(&interface2, NULL, tx, rx, sizeof(rx));
 
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS, e);
+	zassert_equal(SID_ERROR_INVALID_ARGS, e);
 	uint8_t rx_expect[5] = { 0 };
 
-	TEST_ASSERT_EQUAL_MEMORY(rx_expect, rx, sizeof(rx));
+	zassert_mem_equal(rx_expect, rx, sizeof(rx));
 }
 
-void test_send_spi()
+ZTEST(spi_bus, test_send_spi)
 {
 	const struct sid_pal_serial_bus_iface *interface = NULL;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
-	TEST_ASSERT(interface);
-	TEST_ASSERT(interface->xfer);
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_not_null(interface);
+	zassert_not_null(interface->xfer);
 	uint8_t tx[] = { 0x1d, 0x08, 0xac, 0, 0 };
 	uint8_t rx[5] = { 0 };
 	uint8_t invalid_rx[5] = { 0 };
 	sid_error_t e = interface->xfer(interface, &client, tx, rx, sizeof(rx));
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, e);
+	zassert_equal(SID_ERROR_NONE, e);
 	for (int i = 0; i < ARRAY_SIZE(rx); i++) {
 		printf("%x ", rx[i]);
 	}
-	TEST_ASSERT(memcmp(rx, invalid_rx, ARRAY_SIZE(rx)) != 0);
+	zassert_true(memcmp(rx, invalid_rx, ARRAY_SIZE(rx)) != 0);
 }
 
-void test_only_tx_spi()
+ZTEST(spi_bus, test_only_tx_spi)
 {
 	const struct sid_pal_serial_bus_iface *interface = NULL;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
-	TEST_ASSERT(interface);
-	TEST_ASSERT(interface->xfer);
+	zassert_equal(SID_ERROR_NONE, sid_pal_serial_bus_nordic_spi_create(&interface, NULL));
+	zassert_not_null(interface);
+	zassert_not_null(interface->xfer);
 	uint8_t tx[] = { 0x1d, 0x08, 0xac };
 	sid_error_t e = interface->xfer(interface, &client, tx, NULL, sizeof(tx));
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, e);
+	zassert_equal(SID_ERROR_NONE, e);
 }
 
-extern int unity_main(void);
-
-int main(void)
+ZTEST(spi_bus, test_sanity)
 {
-	return unity_main();
+	zassert_equal(true, true);
 }
+
+ZTEST_SUITE(spi_bus, NULL, NULL, NULL, NULL, NULL);
