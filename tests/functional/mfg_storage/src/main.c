b432ba0f62643eeb392e0ca45718e4a421951711
Robert Gałat <robert.galat@nordicsemi.no>
2023-07-11 10:20:48 +0000

    tests: port functional/mfg_storage to ztest
    port test to ztest framework
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- tests/functional/mfg_storage/src/main.c
+++ tests/functional/mfg_storage/src/main.c
@@ -3,7 +3,9 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-#include <unity.h>
+
+#include <zephyr/ztest.h>
+
 #include <sid_pal_mfg_store_ifc.h>
 #include <sid_error.h>
 
@@ -63,35 +65,35 @@ static void mfg_set_version(uint32_t version)
 	uint8_t write_buff[SID_PAL_MFG_STORE_MAX_FLASH_WRITE_LEN];
 
 	memcpy(write_buff, &version, SID_PAL_MFG_STORE_VERSION_SIZE);
-	TEST_ASSERT_EQUAL(0, sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff,
-						     SID_PAL_MFG_STORE_VERSION_SIZE));
+	zassert_equal(0, sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff,
+						 SID_PAL_MFG_STORE_VERSION_SIZE));
 	version = sys_cpu_to_be32(version);
-	TEST_ASSERT_EQUAL(version, sid_pal_mfg_store_get_version());
+	zassert_equal(version, sid_pal_mfg_store_get_version());
 }
 
 static void mfg_clr_memory(void)
 {
-	TEST_ASSERT_EQUAL(0, sid_pal_mfg_store_erase());
-	TEST_ASSERT_TRUE(sid_pal_mfg_store_is_empty());
+	zassert_equal(0, sid_pal_mfg_store_erase());
+	zassert_true(sid_pal_mfg_store_is_empty());
 }
 
-void test_sid_pal_mfg_storage_no_init(void)
+ZTEST(mfg_storage, test_1sid_pal_mfg_storage_no_init)
 {
 	uint8_t read_buffer[SID_PAL_MFG_STORE_VERSION_SIZE] = { 0 };
 
-	TEST_ASSERT_EQUAL(SID_ERROR_UNINITIALIZED,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, read_buffer,
-						  SID_PAL_MFG_STORE_VERSION_SIZE));
-	TEST_ASSERT_EQUAL(SID_ERROR_UNINITIALIZED, sid_pal_mfg_store_erase());
-	TEST_ASSERT_FALSE(sid_pal_mfg_store_is_empty());
+	zassert_equal(SID_ERROR_UNINITIALIZED,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, read_buffer,
+					      SID_PAL_MFG_STORE_VERSION_SIZE));
+	zassert_equal(SID_ERROR_UNINITIALIZED, sid_pal_mfg_store_erase());
+	zassert_false(sid_pal_mfg_store_is_empty());
 	memset(read_buffer, 0xAA, SID_PAL_MFG_STORE_VERSION_SIZE);
 	memset(test_data_buffer, 0xAA, SID_PAL_MFG_STORE_VERSION_SIZE);
 	sid_pal_mfg_store_read(SID_PAL_MFG_STORE_VERSION, read_buffer,
 			       SID_PAL_MFG_STORE_VERSION_SIZE);
-	TEST_ASSERT_EQUAL_HEX8_ARRAY(read_buffer, test_data_buffer, SID_PAL_MFG_STORE_VERSION_SIZE);
+	zassert_mem_equal(read_buffer, test_data_buffer, SID_PAL_MFG_STORE_VERSION_SIZE);
 }
 
-void test_sid_pal_mfg_storage_init(void)
+ZTEST(mfg_storage, test_2sid_pal_mfg_storage_init)
 {
 	static const sid_pal_mfg_store_region_t mfg_store_region = {
 		.addr_start = MFG_START_OFFSET,
@@ -99,45 +101,45 @@ void test_sid_pal_mfg_storage_init(void)
 	};
 
 	sid_pal_mfg_store_init(mfg_store_region);
-	TEST_ASSERT_EQUAL(0, sid_pal_mfg_store_erase());
+	zassert_equal(0, sid_pal_mfg_store_erase());
 }
 
-void test_sid_pal_mfg_storage_write(void)
+ZTEST(mfg_storage, test_3sid_pal_mfg_storage_write)
 {
 	uint8_t write_buff[SID_PAL_MFG_STORE_MAX_FLASH_WRITE_LEN] = { 0 };
 
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 0));
-	TEST_ASSERT_EQUAL(SID_ERROR_OUT_OF_RESOURCES,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 128));
-	TEST_ASSERT_EQUAL(SID_ERROR_INCOMPATIBLE_PARAMS,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 3));
-	TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-			  sid_pal_mfg_store_write(999, write_buff, SID_PAL_MFG_STORE_VERSION_SIZE));
-	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, NULL,
-						  SID_PAL_MFG_STORE_VERSION_SIZE));
+	zassert_equal(SID_ERROR_INVALID_ARGS,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 0));
+	zassert_equal(SID_ERROR_OUT_OF_RESOURCES,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 128));
+	zassert_equal(SID_ERROR_INCOMPATIBLE_PARAMS,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, write_buff, 3));
+	zassert_equal(SID_ERROR_NOT_FOUND,
+		      sid_pal_mfg_store_write(999, write_buff, SID_PAL_MFG_STORE_VERSION_SIZE));
+	zassert_equal(SID_ERROR_NULL_POINTER,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_VERSION, NULL,
+					      SID_PAL_MFG_STORE_VERSION_SIZE));
 
 	mfg_set_version(MFG_VERSION_1_VAL);
 }
 
-void test_sid_pal_mfg_storage_read(void)
+ZTEST(mfg_storage, test_4sid_pal_mfg_storage_read)
 {
 	uint32_t version = MFG_VERSION_1_VAL;
 	uint8_t read_buff[64];
 
 	sid_pal_mfg_store_read(SID_PAL_MFG_STORE_VERSION, read_buff,
 			       SID_PAL_MFG_STORE_VERSION_SIZE);
-	TEST_ASSERT_EQUAL_MEMORY(&version, read_buff, SID_PAL_MFG_STORE_VERSION_SIZE);
+	zassert_mem_equal(&version, read_buff, SID_PAL_MFG_STORE_VERSION_SIZE);
 }
 
-void test_sid_pal_mfg_storage_erase(void)
+ZTEST(mfg_storage, test_5sid_pal_mfg_storage_erase)
 {
-	TEST_ASSERT_FALSE(sid_pal_mfg_store_is_empty());
+	zassert_false(sid_pal_mfg_store_is_empty());
 	mfg_clr_memory();
 }
 
-void test_sid_pal_mfg_storage_dev_id_get(void)
+ZTEST(mfg_storage, test_6sid_pal_mfg_storage_dev_id_get)
 {
 	uint8_t dev_id[SID_PAL_MFG_STORE_DEVID_SIZE];
 	uint8_t fake_dev_id[8] = { 0xAC, 0xBC, 0xCC, 0xDC, 0x11, 0x12, 0x13, 0x14 };
@@ -145,22 +147,22 @@ void test_sid_pal_mfg_storage_dev_id_get(void)
 	memset(dev_id, 0x00, sizeof(dev_id));
 	mfg_clr_memory();
 
-	TEST_ASSERT_FALSE(sid_pal_mfg_store_dev_id_get(dev_id));
-	TEST_ASSERT_EQUAL(0xBF, dev_id[0]);
+	zassert_false(sid_pal_mfg_store_dev_id_get(dev_id));
+	zassert_equal(0xBF, dev_id[0]);
 	// dev id is unique for every chip, so we cannot verify it in another way:
-	TEST_ASSERT_NOT_EQUAL(0x00, dev_id[1]);
-	TEST_ASSERT_NOT_EQUAL(0x00, dev_id[2]);
-	TEST_ASSERT_NOT_EQUAL(0x00, dev_id[3]);
-	TEST_ASSERT_NOT_EQUAL(0x00, dev_id[4]);
+	zassert_not_equal(0x00, dev_id[1]);
+	zassert_not_equal(0x00, dev_id[2]);
+	zassert_not_equal(0x00, dev_id[3]);
+	zassert_not_equal(0x00, dev_id[4]);
 
 	memset(dev_id, 0x00, sizeof(dev_id));
 	// Set fake dev_id.
-	TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-			  sid_pal_mfg_store_write(SID_PAL_MFG_STORE_DEVID, fake_dev_id,
-						  sizeof(fake_dev_id)));
+	zassert_equal(SID_ERROR_NOT_FOUND,
+		      sid_pal_mfg_store_write(SID_PAL_MFG_STORE_DEVID, fake_dev_id,
+					      sizeof(fake_dev_id)));
 }
 
-void test_sid_pal_mfg_storage_sn_get(void)
+ZTEST(mfg_storage, test_7sid_pal_mfg_storage_sn_get)
 {
 	uint8_t serial_num[SID_PAL_MFG_STORE_SERIAL_NUM_SIZE];
 	uint8_t fake_serial_num[20];
@@ -172,26 +174,24 @@ void test_sid_pal_mfg_storage_sn_get(void)
 	mfg_clr_memory();
 
 	// No serial number.
-	TEST_ASSERT_FALSE(sid_pal_mfg_store_serial_num_get(serial_num));
+	zassert_false(sid_pal_mfg_store_serial_num_get(serial_num));
 
 	// Set fake serial number.
-	TEST_ASSERT_EQUAL(0, sid_pal_mfg_store_write(SID_PAL_MFG_STORE_SERIAL_NUM, fake_serial_num,
-						     sizeof(fake_serial_num)));
-	TEST_ASSERT_TRUE(sid_pal_mfg_store_serial_num_get(serial_num));
-	TEST_ASSERT_EQUAL_HEX8_ARRAY(fake_serial_num, serial_num,
-				     SID_PAL_MFG_STORE_SERIAL_NUM_SIZE);
+	zassert_equal(0, sid_pal_mfg_store_write(SID_PAL_MFG_STORE_SERIAL_NUM, fake_serial_num,
+						 sizeof(fake_serial_num)));
+	zassert_true(sid_pal_mfg_store_serial_num_get(serial_num));
+	zassert_mem_equal(fake_serial_num, serial_num, SID_PAL_MFG_STORE_SERIAL_NUM_SIZE);
 
 	// Set version to MFG_VERSION_1_VAL
 	mfg_set_version(MFG_VERSION_1_VAL);
-	TEST_ASSERT_TRUE(sid_pal_mfg_store_serial_num_get(serial_num));
+	zassert_true(sid_pal_mfg_store_serial_num_get(serial_num));
 	hton_buff(serial_num, SID_PAL_MFG_STORE_SERIAL_NUM_SIZE);
-	TEST_ASSERT_EQUAL_HEX8_ARRAY(fake_serial_num, serial_num,
-				     SID_PAL_MFG_STORE_SERIAL_NUM_SIZE);
+	zassert_mem_equal(fake_serial_num, serial_num, SID_PAL_MFG_STORE_SERIAL_NUM_SIZE);
 }
 
-extern int unity_main(void);
-
-int main(void)
+ZTEST(mfg_storage, test_sanity)
 {
-	return unity_main();
+	zassert_equal(true, true);
 }
+
+ZTEST_SUITE(mfg_storage, NULL, NULL, NULL, NULL, NULL);
