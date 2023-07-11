6f93486510937d47ae65dfc8cd9cf8fac83ca502
Robert Gałat <robert.galat@nordicsemi.no>
2023-07-11 12:00:22 +0000

    tests: port functional/storage to ztest
    port to ztest
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- tests/functional/storage/src/main.c
+++ tests/functional/storage/src/main.c
@@ -3,7 +3,9 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-#include <unity.h>
+
+#include <zephyr/ztest.h>
+
 #include <sid_pal_storage_kv_ifc.h>
 
 #include <zephyr/storage/flash_map.h>
@@ -36,89 +38,89 @@ static uint8_t test_data_buffer[512];
 static uint32_t test_read_data;
 static uint8_t test_key_x = 0xCC;
 
-static void test_sid_pal_storage_init(void)
+static void *sid_pal_storage_test_init(void)
 {
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
 		memset(&test_data[tk], 0x05 + tk, sizeof(uint32_t));
 	}
+	return NULL;
 }
 
 /******************************************************************
 * NOTE: DO NOT CHANGE THE ORDER OF THE TESTS!
 * ****************************************************************/
-void test_sid_pal_storage_no_init(void)
+ZTEST(functional_storage, test_1sid_pal_storage_no_init)
 {
 	uint32_t len;
 
 	// NVM is not initialized.
-	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_ERASE_FAIL,
-			  sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
-	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_WRITE_FAIL,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1, &test_data[0],
-							sizeof(test_data[0])));
-	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_READ_FAIL,
-			  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, TEST_KEY_1,
-							&test_read_data, sizeof(test_read_data)));
-	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_READ_FAIL,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, TEST_KEY_1, &len));
-	TEST_ASSERT_EQUAL(SID_ERROR_STORAGE_ERASE_FAIL,
-			  sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, TEST_KEY_1));
+	zassert_equal(SID_ERROR_STORAGE_ERASE_FAIL,
+		      sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
+	zassert_equal(SID_ERROR_STORAGE_WRITE_FAIL,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1, &test_data[0],
+						    sizeof(test_data[0])));
+	zassert_equal(SID_ERROR_STORAGE_READ_FAIL,
+		      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, TEST_KEY_1, &test_read_data,
+						    sizeof(test_read_data)));
+	zassert_equal(SID_ERROR_STORAGE_READ_FAIL,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, TEST_KEY_1, &len));
+	zassert_equal(SID_ERROR_STORAGE_ERASE_FAIL,
+		      sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, TEST_KEY_1));
 }
 
-void test_sid_pal_storage_kv_init(void)
+ZTEST(functional_storage, test_2sid_pal_storage_kv_init)
 {
 	sid_pal_storage_kv_init();
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_init());
+	zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_init());
 }
 
-void test_sid_pal_storage_incorrect_group_id(void)
+ZTEST(functional_storage, test_3sid_pal_storage_incorrect_group_id)
 {
 	uint32_t len;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE,
-			  sid_pal_storage_kv_group_delete(GROUP_ID_TEST_NOK));
-	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_NOK, TEST_KEY_1,
-							&test_data[TEST_KEY_1],
-							sizeof(test_data[TEST_KEY_1])));
-	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE,
-			  sid_pal_storage_kv_record_get(GROUP_ID_TEST_NOK, TEST_KEY_1,
-							&test_read_data, sizeof(test_read_data)));
-	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_NOK, TEST_KEY_1, &len));
-	TEST_ASSERT_EQUAL(SID_ERROR_PARAM_OUT_OF_RANGE,
-			  sid_pal_storage_kv_record_delete(GROUP_ID_TEST_NOK, TEST_KEY_1));
+	zassert_equal(SID_ERROR_PARAM_OUT_OF_RANGE,
+		      sid_pal_storage_kv_group_delete(GROUP_ID_TEST_NOK));
+	zassert_equal(SID_ERROR_PARAM_OUT_OF_RANGE,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_NOK, TEST_KEY_1,
+						    &test_data[TEST_KEY_1],
+						    sizeof(test_data[TEST_KEY_1])));
+	zassert_equal(SID_ERROR_PARAM_OUT_OF_RANGE,
+		      sid_pal_storage_kv_record_get(GROUP_ID_TEST_NOK, TEST_KEY_1, &test_read_data,
+						    sizeof(test_read_data)));
+	zassert_equal(SID_ERROR_PARAM_OUT_OF_RANGE,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_NOK, TEST_KEY_1, &len));
+	zassert_equal(SID_ERROR_PARAM_OUT_OF_RANGE,
+		      sid_pal_storage_kv_record_delete(GROUP_ID_TEST_NOK, TEST_KEY_1));
 }
 
-void test_sid_pal_storage_null_ptr(void)
+ZTEST(functional_storage, test_4sid_pal_storage_null_ptr)
 {
-	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1, NULL,
-							sizeof(test_data[TEST_KEY_1])));
-	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER,
-			  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, TEST_KEY_1, NULL,
-							sizeof(test_read_data)));
-	TEST_ASSERT_EQUAL(SID_ERROR_NULL_POINTER,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, TEST_KEY_1, NULL));
+	zassert_equal(SID_ERROR_NULL_POINTER,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1, NULL,
+						    sizeof(test_data[TEST_KEY_1])));
+	zassert_equal(SID_ERROR_NULL_POINTER,
+		      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, TEST_KEY_1, NULL,
+						    sizeof(test_read_data)));
+	zassert_equal(SID_ERROR_NULL_POINTER,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, TEST_KEY_1, NULL));
 }
 
-void test_sid_pal_storage_erase_all_1(void)
+ZTEST(functional_storage, test_5sid_pal_storage_erase_all_1)
 {
 	test_read_data = 0;
 	uint32_t len;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
+	zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-								&test_read_data,
-								sizeof(test_read_data)));
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
 	}
 }
 
-void test_sid_pal_storage_write_get(void)
+ZTEST(functional_storage, test_6sid_pal_storage_write_get)
 {
 	test_read_data = 0;
 	uint32_t sector_size = 0x1000;
@@ -130,166 +132,159 @@ void test_sid_pal_storage_write_get(void)
 	memset(update_val, 0x12, sizeof(update_val));
 
 	// Incorrect length
-	TEST_ASSERT_EQUAL(SID_ERROR_INVALID_ARGS,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1,
-							&test_data[TEST_KEY_1], 0));
+	zassert_equal(SID_ERROR_INVALID_ARGS,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_1,
+						    &test_data[TEST_KEY_1], 0));
 
 	// Write many data
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_set(
-							  GROUP_ID_TEST_OK, tk, &test_data[tk],
-							  sizeof(test_data[tk])));
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, tk, &test_data[tk],
+							    sizeof(test_data[tk])));
 	}
 	// Check data consistency
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
-		TEST_ASSERT_EQUAL(sizeof(test_data[tk]), len);
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
-							  GROUP_ID_TEST_OK, tk, &test_read_data,
-							  sizeof(test_read_data)));
-		TEST_ASSERT_EQUAL(test_read_data, test_data[tk]);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(sizeof(test_data[tk]), len);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
+		zassert_equal(test_read_data, test_data[tk]);
 	}
 
 	// Now, try to write more data.
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_set(
-						  GROUP_ID_TEST_OK, test_key_x, test_data_buffer,
-						  sizeof(test_data_buffer)));
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
-	TEST_ASSERT_EQUAL(sizeof(test_data_buffer), len);
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, test_key_x, test_data_buffer,
+						    sizeof(test_data_buffer)));
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
+	zassert_equal(sizeof(test_data_buffer), len);
 
 	// Check if we do not out of the memory
-	TEST_ASSERT_EQUAL(SID_ERROR_OUT_OF_RESOURCES,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, test_key_x,
-							test_data_buffer, sector_size));
+	zassert_equal(SID_ERROR_OUT_OF_RESOURCES,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, test_key_x, test_data_buffer,
+						    sector_size));
 
 	// Check again data consistency
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
-		TEST_ASSERT_EQUAL(sizeof(test_data[tk]), len);
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
-							  GROUP_ID_TEST_OK, tk, &test_read_data,
-							  sizeof(test_read_data)));
-		TEST_ASSERT_EQUAL(test_read_data, test_data[tk]);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(sizeof(test_data[tk]), len);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
+		zassert_equal(test_read_data, test_data[tk]);
 	}
 
 	// Update one key
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-			  sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_5, update_val,
-							sizeof(update_val)));
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, TEST_KEY_5, update_val,
+						    sizeof(update_val)));
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
 		if (TEST_KEY_5 == tk) {
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
-								  GROUP_ID_TEST_OK, tk, &len));
-			TEST_ASSERT_EQUAL(sizeof(update_val), len);
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-					  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-									update_val_read,
-									sizeof(update_val_read)));
-			TEST_ASSERT_EQUAL_HEX8_ARRAY(update_val_read, update_val,
-						     sizeof(update_val_read));
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
+							      GROUP_ID_TEST_OK, tk, &len));
+			zassert_equal(sizeof(update_val), len);
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
+							      GROUP_ID_TEST_OK, tk, update_val_read,
+							      sizeof(update_val_read)));
+			zassert_mem_equal(update_val_read, update_val, sizeof(update_val_read));
 		} else {
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
-								  GROUP_ID_TEST_OK, tk, &len));
-			TEST_ASSERT_EQUAL(sizeof(test_data[tk]), len);
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-					  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-									&test_read_data,
-									sizeof(test_read_data)));
-			TEST_ASSERT_EQUAL(test_read_data, test_data[tk]);
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
+							      GROUP_ID_TEST_OK, tk, &len));
+			zassert_equal(sizeof(test_data[tk]), len);
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
+							      GROUP_ID_TEST_OK, tk, &test_read_data,
+							      sizeof(test_read_data)));
+			zassert_equal(test_read_data, test_data[tk]);
 		}
 	}
 
 	// Restore data
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_set(
-							  GROUP_ID_TEST_OK, tk, &test_data[tk],
-							  sizeof(test_data[tk])));
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_set(GROUP_ID_TEST_OK, tk, &test_data[tk],
+							    sizeof(test_data[tk])));
 	}
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
-		TEST_ASSERT_EQUAL(sizeof(test_data[tk]), len);
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
-							  GROUP_ID_TEST_OK, tk, &test_read_data,
-							  sizeof(test_read_data)));
-		TEST_ASSERT_EQUAL(test_read_data, test_data[tk]);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(sizeof(test_data[tk]), len);
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
+		zassert_equal(test_read_data, test_data[tk]);
 	}
 }
 
-void test_sid_pal_storage_delete_records(void)
+ZTEST(functional_storage, test_7sid_pal_storage_delete_records)
 {
 	test_read_data = 0;
 	uint32_t len;
 
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-			  sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, TEST_KEY_5));
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, TEST_KEY_5));
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
 		if (TEST_KEY_5 == tk) {
-			TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-					  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-									&test_read_data,
-									sizeof(test_read_data)));
+			zassert_equal(SID_ERROR_NOT_FOUND,
+				      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
+								    &test_read_data,
+								    sizeof(test_read_data)));
 		} else {
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
-								  GROUP_ID_TEST_OK, tk, &len));
-			TEST_ASSERT_EQUAL(sizeof(test_data[tk]), len);
-			TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-					  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-									&test_read_data,
-									sizeof(test_read_data)));
-			TEST_ASSERT_EQUAL(test_read_data, test_data[tk]);
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get_len(
+							      GROUP_ID_TEST_OK, tk, &len));
+			zassert_equal(sizeof(test_data[tk]), len);
+			zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_record_get(
+							      GROUP_ID_TEST_OK, tk, &test_read_data,
+							      sizeof(test_read_data)));
+			zassert_equal(test_read_data, test_data[tk]);
 		}
 	}
 
 	// another keys
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-				  sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, tk));
+		zassert_equal(SID_ERROR_NONE,
+			      sid_pal_storage_kv_record_delete(GROUP_ID_TEST_OK, tk));
 	}
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-								&test_read_data,
-								sizeof(test_read_data)));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
 	}
 }
 
-void test_sid_pal_storage_erase_all_2(void)
+ZTEST(functional_storage, test_8sid_pal_storage_erase_all_2)
 {
 	test_read_data = 0;
 	uint32_t len;
 
 	// Check if 'test_key_x' key still exist
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
-	TEST_ASSERT_EQUAL(sizeof(test_data_buffer), len);
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE,
-			  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, test_key_x,
-							&test_read_data, sizeof(test_read_data)));
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
+	zassert_equal(sizeof(test_data_buffer), len);
+	zassert_equal(SID_ERROR_NONE,
+		      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, test_key_x, &test_read_data,
+						    sizeof(test_read_data)));
 
 	// Erase whole NVM
-	TEST_ASSERT_EQUAL(SID_ERROR_NONE, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
+	zassert_equal(SID_ERROR_NONE, sid_pal_storage_kv_group_delete(GROUP_ID_TEST_OK));
 	for (test_keys_t tk = 0; tk < TEST_KEY_END; tk++) {
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk,
-								&test_read_data,
-								sizeof(test_read_data)));
-		TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-				  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get(GROUP_ID_TEST_OK, tk, &test_read_data,
+							    sizeof(test_read_data)));
+		zassert_equal(SID_ERROR_NOT_FOUND,
+			      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, tk, &len));
 	}
-	TEST_ASSERT_EQUAL(SID_ERROR_NOT_FOUND,
-			  sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
+	zassert_equal(SID_ERROR_NOT_FOUND,
+		      sid_pal_storage_kv_record_get_len(GROUP_ID_TEST_OK, test_key_x, &len));
 }
 
-extern int unity_main(void);
-
-int main(void)
+ZTEST(functional_storage, test_sanity)
 {
-	test_sid_pal_storage_init();
-	return unity_main();
+	zassert_equal(true, true);
 }
+
+ZTEST_SUITE(functional_storage, NULL, sid_pal_storage_test_init, NULL, NULL, NULL);
