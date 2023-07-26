9673888fabffd6d556c932007abd2cdb87a2add5
Robert Gałat <robert.galat@nordicsemi.no>
2023-07-26 11:36:21 +0000

    Build: update requirements
    Python requirements are causing issues during clean install
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -11,10 +11,5 @@ if [ ! -f $HOME/.devcontainer_initalized ]; then
     bash /work/ncs/sidewalk/scripts/git_hooks/install_hooks.sh
     pip install -r /work/ncs/sidewalk/requirements.txt
 
-    if [ ! -f /work/ncs/sidewalk/doc/venv/bin/activate ]; then
-        cd /work/ncs/sidewalk/doc/
-        python3 -m venv venv
-        /bin/bash -c ". /work/ncs/sidewalk/doc/venv/bin/activate && pip install -r /work/ncs/sidewalk/doc/requirements-doc.txt"
-    fi
     touch $HOME/.devcontainer_initalized
 fi
