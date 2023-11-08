8279275ab130bda388312e5d9623f2dd57e3261a
Anna Wojdylo <anna.wojdylo@nordicsemi.no>
2023-11-08 13:38:11 +0100

    doc: included previous known issues
    Added previous known issues per release.
    Added toggle extension.
    
    Signed-off-by: Anna Wojdylo <anna.wojdylo@nordicsemi.no>

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,11 @@ author = 'Nordic Semiconductor'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx_tabs.tabs']
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs',
+    'sphinx_togglebutton',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
