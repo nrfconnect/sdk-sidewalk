808dcd0e0fe3e75772d3d7973440565767d55f1f
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-12-11 17:57:21 +0100

    sample: add sidewalk minimal init
    KRKNWK-17953
    TBD
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/app/src/main.c
+++ samples/app/src/main.c
@@ -4,7 +4,10 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <stdio.h>
+#include <sid_api.h>
+#include <pal_init.h>
+#include <app_ble_config.h>
+#include <app_subGHz_config.h>
 #include <dk_buttons_and_leds.h>
 #include <zephyr/kernel.h>
 #include <zephyr/smf.h>
@@ -12,6 +15,113 @@
 
 LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
+/* Sidewalk Thread*/
+K_THREAD_STACK_DEFINE(sidewalk_work_q_stack, KB(10));
+
+struct sid_ctx_s {
+	struct k_work sid_event_work;
+	struct k_work_q sid_work_q;
+	struct sid_handle *sid_handle;
+	struct sid_config config;
+};
+
+static struct sid_ctx_s sid_ctx = { 0 };
+
+static void on_sidewalk_event(bool in_isr, void *context);
+static void on_sidewalk_msg_received(const struct sid_msg_desc *msg_desc, const struct sid_msg *msg,
+				     void *context);
+static void on_sidewalk_msg_sent(const struct sid_msg_desc *msg_desc, void *context);
+static void on_sidewalk_send_error(sid_error_t error, const struct sid_msg_desc *msg_desc,
+				   void *context);
+static void on_sidewalk_factory_reset(void *context);
+static void on_sidewalk_status_changed(const struct sid_status *status, void *context);
+
+static struct sid_event_callbacks event_callbacks = {
+	.context = &sid_ctx,
+	.on_event = on_sidewalk_event,
+	.on_msg_received = on_sidewalk_msg_received,
+	.on_msg_sent = on_sidewalk_msg_sent,
+	.on_send_error = on_sidewalk_send_error,
+	.on_status_changed = on_sidewalk_status_changed,
+	.on_factory_reset = on_sidewalk_factory_reset,
+};
+
+static void sidewalk_event_worker(struct k_work *work)
+{
+	struct sid_ctx_s *sid_ctx = CONTAINER_OF(work, struct sid_ctx_s, sid_event_work);
+
+	sid_error_t e = sid_process(sid_ctx->sid_handle);
+
+	if (e) {
+		LOG_ERR("sid process error %d", e);
+	}
+}
+
+static void on_sidewalk_event(bool in_isr, void *context)
+{
+	struct sid_ctx_s *ctx = (struct sid_ctx_s *)context;
+
+	int e = k_work_submit_to_queue(&ctx->sid_work_q, &ctx->sid_event_work);
+
+	if (e) {
+		LOG_ERR("sid process queue error %d", e);
+	}
+}
+
+int sidewalk_thread_init(void *context)
+{
+	struct sid_ctx_s *ctx = (struct sid_ctx_s *)context;
+
+	k_work_queue_init(&ctx->sid_work_q);
+	k_work_queue_start(&ctx->sid_work_q, sidewalk_work_q_stack,
+			   K_THREAD_STACK_SIZEOF(sidewalk_work_q_stack),
+			   CONFIG_SIDEWALK_THREAD_PRIORITY, NULL);
+	k_work_init(&ctx->sid_event_work, sidewalk_event_worker);
+
+	ctx->config = (struct sid_config){
+		.callbacks = &event_callbacks,
+		.time_sync_periodicity_seconds = 7200,
+		.link_config = app_get_ble_config(),
+		.sub_ghz_link_config = app_get_sub_ghz_config(),
+	};
+
+	sid_error_t e = application_pal_init();
+	if (e) {
+		LOG_ERR("sid pal init error %d", e);
+	}
+
+	return e;
+}
+
+/* Sidewalk Events */
+static void on_sidewalk_msg_received(const struct sid_msg_desc *msg_desc, const struct sid_msg *msg,
+				     void *context)
+{
+	LOG_INF("%s", __func__);
+}
+
+static void on_sidewalk_msg_sent(const struct sid_msg_desc *msg_desc, void *context)
+{
+	LOG_INF("%s", __func__);
+}
+
+static void on_sidewalk_send_error(sid_error_t error, const struct sid_msg_desc *msg_desc,
+				   void *context)
+{
+	LOG_INF("%s", __func__);
+}
+
+static void on_sidewalk_factory_reset(void *context)
+{
+	LOG_INF("%s", __func__);
+}
+
+static void on_sidewalk_status_changed(const struct sid_status *status, void *context)
+{
+	LOG_INF("%s", __func__);
+}
+
+/* Application State Machine */
 #define EVENT_BTN_PRESS BIT(0)
 
 static void state_unregistered_run(void *o);
@@ -92,6 +202,10 @@ int main(void)
 
 	if (dk_buttons_init(button_changed)) {
 		LOG_ERR("Cannot init buttons");
+	}
+
+	if (sidewalk_thread_init(&sid_ctx)) {
+		LOG_ERR("Cannot init sid thread");
 	}
 
 	k_event_init(&s_obj.smf_event);
