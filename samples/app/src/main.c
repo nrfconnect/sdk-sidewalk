afed1c98dc1041547907c90c1eee209391a5fb4a
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2023-12-14 13:57:38 +0100

    sample: app events, fix ctx
    KRKNWK-17953
    tbd
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/app/src/main.c
+++ samples/app/src/main.c
@@ -18,14 +18,15 @@ LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 static struct k_thread app_thread;
 K_THREAD_STACK_DEFINE(app_thread_stack, CONFIG_SIDEWALK_THREAD_STACK_SIZE);
 
-typedef enum app_events {
-	APP_EVENT_SIDEWALK,
-	APP_EVENT_START,
-	APP_EVENT_SEND_HELLO,
-	APP_EVENT_FACTORY_RESET,
-	APP_EVENT_FSK_CSS_SWITCH,
-	APP_EVENT_SET_DEVICE_PROFILE
-} app_event_t;
+#define APP_EVENT_SIDEWALK BIT(0)
+#define APP_EVENT_STATE_UPDATE BIT(1)
+#define APP_EVENT_SEND_HELLO BIT(2)
+#define APP_EVENT_FACTORY_RESET BIT(3)
+#define APP_EVENT_FSK_CSS_SWITCH BIT(4)
+#define APP_EVENT_SET_DEVICE_PROFILE BIT(5)
+#define APP_EVENT_ALL                                                                              \
+	(APP_EVENT_SIDEWALK | APP_EVENT_STATE_UPDATE | APP_EVENT_SEND_HELLO |                      \
+	 APP_EVENT_FACTORY_RESET | APP_EVENT_FSK_CSS_SWITCH | APP_EVENT_SET_DEVICE_PROFILE)
 
 enum app_state {
 	STATE_INIT,
@@ -36,12 +37,12 @@ enum app_state {
 
 struct app_sm {
 	struct smf_ctx ctx;
-	struct k_msgq app_msgq;
-	app_event_t app_event;
+	struct k_event app_event;
+	int32_t events;
 };
 
 struct sid_ctx_s {
-	struct app_sm sm_obj;
+	struct app_sm sm;
 	struct sid_handle *handle;
 	struct sid_config config;
 };
@@ -59,7 +60,7 @@ static const struct smf_state app_states[] = {
 		SMF_CREATE_STATE(NULL, state_secure_connection_run, NULL),
 };
 
-uint8_t __aligned(4) app_msgq_buffer[CONFIG_SIDEWALK_THREAD_QUEUE_SIZE * sizeof(app_event_t)];
+static struct sid_ctx_s sid_ctx;
 
 static void on_sidewalk_event(bool in_isr, void *context)
 {
@@ -67,10 +68,7 @@ static void on_sidewalk_event(bool in_isr, void *context)
 
 	struct sid_ctx_s *ctx = (struct sid_ctx_s *)context;
 
-	static app_event_t sidewalk_event = APP_EVENT_SIDEWALK;
-	if (k_msgq_put(&ctx->sm_obj.app_msgq, (void *)&sidewalk_event, K_NO_WAIT)) {
-		LOG_ERR("Cannot put message");
-	}
+	(void)k_event_post(&ctx->sm.app_event, APP_EVENT_SIDEWALK);
 }
 
 static void on_sidewalk_msg_received(const struct sid_msg_desc *msg_desc, const struct sid_msg *msg,
@@ -103,54 +101,56 @@ static void on_sidewalk_status_changed(const struct sid_status *status, void *co
 
 	switch (status->state) {
 	case SID_STATE_READY:
-		smf_set_state(SMF_CTX(&ctx->sm_obj), &app_states[STATE_SIDEWALK_READY]);
+		smf_set_state(SMF_CTX(&ctx->sm), &app_states[SID_STATE_READY]);
 		break;
 	case SID_STATE_NOT_READY:
-		smf_set_state(SMF_CTX(&ctx->sm_obj), &app_states[STATE_SIDEWALK_NOT_READY]);
+		smf_set_state(SMF_CTX(&ctx->sm), &app_states[STATE_SIDEWALK_NOT_READY]);
 		break;
 	case SID_STATE_ERROR:
 		LOG_ERR("sidewalk error: %d", (int)sid_get_error(ctx->handle));
 		break;
 	case SID_STATE_SECURE_CHANNEL_READY:
-		smf_set_state(SMF_CTX(&ctx->sm_obj), &app_states[STATE_SIDEWALK_SECURE_CONNECTION]);
+		smf_set_state(SMF_CTX(&ctx->sm), &app_states[STATE_SIDEWALK_SECURE_CONNECTION]);
 		break;
 	default:
 		LOG_ERR("sidewalk unknow state: %d", status->state);
 		break;
 	}
+
+	(void)k_event_post(&ctx->sm.app_event, APP_EVENT_STATE_UPDATE);
 }
 
 static void state_init_run(void *o)
 {
 	LOG_INF("state: init");
 
-	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm_obj);
+	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm);
 	sid_error_t e = SID_ERROR_NONE;
 
-	switch (ctx->sm_obj.app_event) {
-	case APP_EVENT_SIDEWALK:
+	if (ctx->sm.events & APP_EVENT_SEND_HELLO) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_SEND_HELLO);
+		ctx->sm.events &= !APP_EVENT_SEND_HELLO;
+
+		LOG_INF("event: hello");
+	}
+
+	if (ctx->sm.events & APP_EVENT_SIDEWALK) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_SIDEWALK);
+		ctx->sm.events &= !APP_EVENT_SIDEWALK;
+
 		LOG_INF("event: sidewalk");
+
 		e = sid_process(ctx->handle);
 		if (e) {
 			LOG_ERR("sid process err %d", (int)e);
 		}
-		break;
-	case APP_EVENT_START:
-		LOG_INF("event: start");
+	}
 
-		e = sid_init(&ctx->config, &ctx->handle);
-		if (e) {
-			LOG_ERR("sid init err %d", (int)e);
-		}
-		e = sid_start(ctx->handle, SID_LINK_TYPE_1);
-		if (e) {
-			LOG_ERR("sid start err %d", (int)e);
-		}
-		break;
+	if (ctx->sm.events & APP_EVENT_STATE_UPDATE) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_STATE_UPDATE);
+		ctx->sm.events &= !APP_EVENT_STATE_UPDATE;
 
-	default:
-		LOG_ERR("event: unsupported");
-		break;
+		LOG_INF("event: state update");
 	}
 }
 
@@ -158,21 +158,26 @@ static void state_ready_run(void *o)
 {
 	LOG_INF("state: ready");
 
-	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm_obj);
+	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm);
 	sid_error_t e = SID_ERROR_NONE;
 
-	switch (ctx->sm_obj.app_event) {
-	case APP_EVENT_SIDEWALK:
+	if (ctx->sm.events & APP_EVENT_SIDEWALK) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_SIDEWALK);
+		ctx->sm.events &= !APP_EVENT_SIDEWALK;
+
 		LOG_INF("event: sidewalk");
+
 		e = sid_process(ctx->handle);
 		if (e) {
 			LOG_ERR("sid process err %d", (int)e);
 		}
-		break;
+	}
 
-	default:
-		LOG_ERR("event: unsupported");
-		break;
+	if (ctx->sm.events & APP_EVENT_STATE_UPDATE) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_STATE_UPDATE);
+		ctx->sm.events &= !APP_EVENT_STATE_UPDATE;
+
+		LOG_INF("event: state update");
 	}
 }
 
@@ -180,21 +185,33 @@ static void state_not_ready_run(void *o)
 {
 	LOG_INF("state: not ready");
 
-	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm_obj);
+	struct sid_ctx_s *ctx = CONTAINER_OF(o, struct sid_ctx_s, sm);
 	sid_error_t e = SID_ERROR_NONE;
 
-	switch (ctx->sm_obj.app_event) {
-	case APP_EVENT_SIDEWALK:
+	if (ctx->sm.events & APP_EVENT_SIDEWALK) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_SIDEWALK);
+		ctx->sm.events &= !APP_EVENT_SIDEWALK;
+
 		LOG_INF("event: sidewalk");
+
 		e = sid_process(ctx->handle);
 		if (e) {
 			LOG_ERR("sid process err %d", (int)e);
 		}
-		break;
+	}
 
-	default:
-		LOG_ERR("event: unsupported");
-		break;
+	if (ctx->sm.events & APP_EVENT_STATE_UPDATE) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_STATE_UPDATE);
+		ctx->sm.events &= !APP_EVENT_STATE_UPDATE;
+
+		LOG_INF("event: state update");
+	}
+
+	if (ctx->sm.events & APP_EVENT_SEND_HELLO) {
+		k_event_clear(&ctx->sm.app_event, APP_EVENT_SEND_HELLO);
+		ctx->sm.events &= !APP_EVENT_SEND_HELLO;
+
+		LOG_INF("event: hello");
 	}
 }
 
@@ -209,6 +226,7 @@ static void button_changed(uint32_t button_state, uint32_t has_changed)
 
 	if (buttons & DK_BTN1_MSK) {
 		LOG_INF("button 1");
+		(void)k_event_post(&sid_ctx.sm.app_event, APP_EVENT_SEND_HELLO);
 	}
 }
 
@@ -222,7 +240,7 @@ static void app_thread_entry(void *context, void *unused, void *unused2)
 	struct sid_ctx_s *ctx = (struct sid_ctx_s *)context;
 
 	struct sid_event_callbacks event_callbacks = {
-		.context = &ctx,
+		.context = ctx,
 		.on_event = on_sidewalk_event,
 		.on_msg_received = on_sidewalk_msg_received,
 		.on_msg_sent = on_sidewalk_msg_sent,
@@ -239,28 +257,30 @@ static void app_thread_entry(void *context, void *unused, void *unused2)
 		.sub_ghz_link_config = app_get_sub_ghz_config(),
 	};
 
-	if (application_pal_init()) {
-		LOG_ERR("Cannot init pal");
+	k_event_init(&ctx->sm.app_event);
+	smf_set_initial(SMF_CTX(&ctx->sm), &app_states[STATE_INIT]);
+
+	sid_error_t e = SID_ERROR_NONE;
+	e = application_pal_init();
+	if (e) {
+		LOG_ERR("pal init err %d", (int)e);
+	}
+	e = sid_init(&ctx->config, &ctx->handle);
+	if (e) {
+		LOG_ERR("sid init err %d", (int)e);
+	}
+	e = sid_start(ctx->handle, SID_LINK_TYPE_1);
+	if (e) {
+		LOG_ERR("sid start err %d", (int)e);
 	}
 
-	k_msgq_init(&ctx->sm_obj.app_msgq, app_msgq_buffer, sizeof(app_event_t),
-		    CONFIG_SIDEWALK_THREAD_QUEUE_SIZE);
-
-	app_event_t start_event = APP_EVENT_START;
-	if (k_msgq_put(&ctx->sm_obj.app_msgq, (void *)&start_event, K_NO_WAIT)) {
-		LOG_ERR("Cannot put message");
-	}
-
-	smf_set_initial(SMF_CTX(&ctx->sm_obj), &app_states[STATE_INIT]);
 	while (1) {
-		LOG_INF("waiting for event...");
-		int err = k_msgq_get(&ctx->sm_obj.app_msgq, &ctx->sm_obj.app_event, K_FOREVER);
-		if (!err) {
-			if (smf_run_state(SMF_CTX(&ctx->sm_obj))) {
-				break;
-			}
-		} else {
-			LOG_ERR("msgq err %d", err);
+		LOG_INF("waiting for event 0x%x...", ctx->sm.events);
+		ctx->sm.events = k_event_wait(&ctx->sm.app_event, APP_EVENT_ALL, false, K_FOREVER);
+		LOG_INF("processing event 0x%x", ctx->sm.events);
+
+		if (smf_run_state(SMF_CTX(&ctx->sm))) {
+			break;
 		}
 	}
 
@@ -274,8 +294,6 @@ int main(void)
 	if (dk_buttons_init(button_changed)) {
 		LOG_ERR("Cannot init buttons");
 	}
-
-	struct sid_ctx_s sid_ctx = { 0 };
 
 	(void)k_thread_create(&app_thread, app_thread_stack,
 			      K_THREAD_STACK_SIZEOF(app_thread_stack), app_thread_entry, &sid_ctx,
