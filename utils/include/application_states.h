9f364363d4078cd49397d1157fc7e75ef13fc1d7
Robert Gałat <robert.galat@nordicsemi.no>
2024-04-11 13:39:29 +0000

    build: add gpio configuration for nrf54l15
    allows to initialize FSK/LoRa, but still does not work
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- utils/include/application_states.h
+++ utils/include/application_states.h
@@ -31,14 +31,14 @@
 	X(receiving, gpio1, 3)
 #elif defined(NRF54L15_ENGA_XXAA)
 #define X_APPLICAITON_STATES                                                                       \
-	X(error, gpio1, 4)                                                                         \
+	X(error, gpio0, 4)                                                                         \
 	X(working, gpio1, 14)                                                                      \
 	X(registered, gpio1, 13)                                                                   \
 	X(time_sync, gpio1, 8)                                                                     \
 	X(connected, gpio0, 4)                                                                     \
-	X(dfu, gpio1, 5)                                                                           \
-	X(sending, gpio1, 6)                                                                       \
-	X(receiving, gpio1, 7)
+	X(dfu, gpio0, 4)                                                                           \
+	X(sending, gpio0, 4)                                                                       \
+	X(receiving, gpio0, 4)
 #else
 #error "Unknow device application states."
 #endif
