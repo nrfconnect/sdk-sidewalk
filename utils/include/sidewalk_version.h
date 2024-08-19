b3c40a694b8825f6ebd523a508a67e546a942f28
Robert Gałat <robert.galat@nordicsemi.no>
2024-08-19 12:06:44 +0000

    log: remove script to print version
    All required information is already included in the build.
    There is no need for external script.
    
    Signed-off-by: Robert Gałat <robert.galat@nordicsemi.no>

--- utils/include/sidewalk_version.h
+++ utils/include/sidewalk_version.h
@@ -7,21 +7,15 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <sid_sdk_version.h>
-
-extern const size_t sidewalk_version_component_count;
-extern const char *const build_time_stamp;
-
-extern const char *const sidewalk_version_component_name[];
-extern const char *const sidewalk_version_component[];
-
-#define NAME_COLUMN_WIDTH "20"
+#include <app_version.h>
 
 #if defined(CONFIG_LOG)
 #define PRINT_SIDEWALK_VERSION()                                                                   \
 	LOG_PRINTK("----------------------------------------------------------------\n");          \
 	LOG_PRINTK("Sidewalk SDK        = %d.%d.%d.%d\n", SID_SDK_MAJOR_VERSION,                   \
 		   SID_SDK_MINOR_VERSION, SID_SDK_PATCH_VERSION, SID_SDK_BUILD_VERSION);           \
-	LOG_PRINTK("build time          = %s\n", build_time_stamp);                                \
+	LOG_PRINTK("APP_BUILD_VERSION   = %s\n", STRINGIFY(APP_BUILD_VERSION));                    \
+	LOG_PRINTK("build time          = %s %s\n", __DATE__, __TIME__);                           \
 	LOG_PRINTK("board               = %s\n", CONFIG_BOARD_TARGET);                             \
 	LOG_PRINTK("----------------------------------------------------------------\n")
 #else
--- version.cmake
+++
@@ -1,7 +0,0 @@
-add_custom_command(
-    OUTPUT  ${PROJECT_BINARY_DIR}/sidewalk_version.c         
-    COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/generate_version_info.py > ${PROJECT_BINARY_DIR}/sidewalk_version.c
-)
-
-zephyr_library()
-zephyr_library_sources(${PROJECT_BINARY_DIR}/sidewalk_version.c)
