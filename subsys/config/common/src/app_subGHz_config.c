4fa30a07cdb6a5dd20600569a83410130208d908
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2025-05-19 09:53:36 +0200

    samples: add RAK4631 support
    [KRKNWK-18801]
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- subsys/config/common/src/app_subGHz_config.c
+++ subsys/config/common/src/app_subGHz_config.c
@@ -119,8 +119,14 @@ static radio_sx126x_device_config_t radio_sx1262_cfg = {
 	.pa_cfg_callback = radio_sx1262_pa_cfg,
 
 	.tcxo = {
-        	.ctrl = SX126X_TCXO_CTRL_NONE,
-    	},
+#if defined(CONFIG_BOARD_RAK4631)
+		.ctrl = SX126X_TCXO_CTRL_DIO3,
+		.voltage = RADIO_SX126X_TCXO_CTRL_1_8V,
+		.timeout = 10,
+#else
+		.ctrl = SX126X_TCXO_CTRL_NONE,
+#endif /* CONFIG_BOARD_RAK4631 */
+	},
 
 	.trim_cap_val_callback = radio_sx1262_trim_val,
 
@@ -131,7 +137,7 @@ static radio_sx126x_device_config_t radio_sx1262_cfg = {
 	},
 
 	.state_timings = {
-		.sleep_to_full_power_us = 406,
+		.sleep_to_full_power_us = 485,
 		.full_power_to_sleep_us = 0,
 		.rx_to_tx_us = 0,
 		.tx_to_rx_us = 0,
@@ -157,14 +163,14 @@ const radio_sx126x_device_config_t *get_radio_cfg(void)
 	radio_sx1262_cfg.gpio_rf_sw_ena =
 		sid_gpio_utils_register_gpio((struct gpio_dt_spec)GPIO_DT_SPEC_GET_OR(
 			DT_NODELABEL(semtech_sx1262_antenna_enable_gpios), gpios, INVALID_DT_GPIO));
-#if CONFIG_SOC_NRF52840
+#if defined(CONFIG_SOC_NRF52840)
 	radio_sx1262_cfg.bus_selector.client_selector =
 		sid_gpio_utils_register_gpio((struct gpio_dt_spec)GPIO_DT_SPEC_GET_OR(
 			DT_NODELABEL(nrfx_spi_cs), gpios, INVALID_DT_GPIO));
 	radio_sx1262_cfg.bus_selector.speed_hz = NRF_SPI_FREQ_8M;
 #else /* CONFIG_SOC_NRF52840 */
 
-	radio_sx1262_cfg.bus_selector.client_selector = 
+	radio_sx1262_cfg.bus_selector.client_selector =
 		sid_gpio_utils_register_gpio(
 			(struct gpio_dt_spec)GPIO_DT_SPEC_GET_OR(DT_NODELABEL(sid_semtech), cs_gpios, INVALID_DT_GPIO));
 	radio_sx1262_cfg.bus_selector.speed_hz =
@@ -189,6 +195,7 @@ const radio_sx126x_device_config_t *get_radio_cfg(void)
 
 struct sid_sub_ghz_links_config sub_ghz_link_config = {
 	.enable_link_metrics = true,
+	.metrics_msg_retries = 3,
 	.sar_dcr = 100,
 	.registration_config = {
 		.enable = true,
