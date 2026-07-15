558b49520050eef0f3a88dad61066f6dcdefb838
Damian Krolik <damian.krolik@nordicsemi.no>
2026-07-15 13:42:44 +0200

    doc: add application development section
    Extract application development section that includes more advanced
    topics for developing production-grade Sidewalk applications.
    
    Add pages about secure storage and bootloader configuration.
    
    Co-authored-by: Anna Wojdylo <anna.wojdylo@nordicsemi.no>
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>
    Signed-off-by: Anna Wojdylo <anna.wojdylo@nordicsemi.no>

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ root_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["venv"]
+exclude_patterns = ["venv", "_build"]
 
 
 # -- Options for HTML output -------------------------------------------------
